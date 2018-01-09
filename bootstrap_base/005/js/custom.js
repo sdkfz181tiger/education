1da95e535b2fe78bbe2327b6ea96247b58b5db84
Shinichi_Ozaki <sdkfz181tiger@gmail.com>
2018-01-09 16:41:29 +0900

    ネタを追加

--- bootstrap_base/005/js/custom.js
+++ bootstrap_base/005/js/custom.js
@@ -4,6 +4,9 @@ window.onload = function(){
 
 	console.log("onload");
 
+	// カウンター初期化
+	let counter = 0;
+
 	// Form
 	$("form").submit(function(){
 		console.log("Submit!!");
@@ -19,5 +22,24 @@ window.onload = function(){
 
 		let check = $("#inputCheck").prop("checked");
 		console.log("check:" + check);
+
+		// カウンターを1つ増やす
+		counter++;
+
+		// Tableに追加する
+		appendInfo(counter, name, email, password);
+
+		return false;
 	});
+}
+
+// Tableに追加する関数
+function appendInfo(counter, name, email, password){
+
+	let th  = '<th scope="row">' + counter + '</th>';
+	let td1 = '<td>' + name + '</td>';
+	let td2 = '<td>' + email + '</td>';
+	let td3 = '<td>' + password + '</td>';
+
+	$("#info").append('<tr>' + th + td1 + td2 + td3 + '</tr>');
 }
