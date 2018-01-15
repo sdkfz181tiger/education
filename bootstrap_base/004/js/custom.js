14bc74b2cfc1852896bf1712c73a59d4241b35cd
Shinichi_Ozaki <sdkfz181tiger@gmail.com>
2018-01-15 13:14:38 +0900

    フォーム整理

--- bootstrap_base/004/js/custom.js
+++ bootstrap_base/004/js/custom.js
@@ -1,11 +1,19 @@
 console.log("custom.js");
 
+const KEY_STORAGE = "mydata";
+
+// データを格納する配列
+let items;
+
 window.onload = function(){
 
 	console.log("onload");
 
-	// カウンター初期化
-	let counter = 0;
+	// データのロード
+	items = loadData();
+
+	// データを表示
+	showData();
 
 	// Form
 	$("form").submit(function(){
@@ -23,23 +31,61 @@ window.onload = function(){
 		let check = $("#inputCheck").prop("checked");
 		console.log("check:" + check);
 
-		// カウンターを1つ増やす
-		counter++;
+		// 1件分のデータを用意する
+		let item = {
+			name: name,
+			email: email,
+			password: password,
+			check: check
+		};
+		items.push(item);
 
-		// Tableに追加する
-		appendInfo(counter, name, email, password);
+		// データのセーブ
+		saveData();
+
+		// データを表示
+		showData();
 
 		return false;
 	});
 }
 
-// Tableに追加する関数
-function appendInfo(counter, name, email, password){
+// データのロード
+function loadData(){
+	// Webstorageから文字列を読み込む
+	let jsonStr = localStorage.getItem(KEY_STORAGE);
+	// JSONオブジェクトに変換する
+	let jsonObj = JSON.parse(jsonStr);
+	if(jsonObj == null) return [];
+	return jsonObj;
+}
 
-	let th  = '<th scope="row">' + counter + '</th>';
-	let td1 = '<td>' + name + '</td>';
-	let td2 = '<td>' + email + '</td>';
-	let td3 = '<td>' + password + '</td>';
+// データのセーブ
+function saveData(){
+	// 配列をJSON文字列に変換する
+	let jsonStr = JSON.stringify(items);
+	// Webstorageに文字列を保存する
+	localStorage.setItem(KEY_STORAGE, jsonStr);
+}
 
-	$("#info").append('<tr>' + th + td1 + td2 + td3 + '</tr>');
+// データを表示する
+function showData(){
+	// テーブルを空にする
+	$("#info").empty();
+	// テーブルに追加する
+	for(let i=0; i<items.length; i++){
+		let th  = '<th scope="row">' + i + '</th>';
+		let td1 = '<td>' + items[i].name + '</td>';
+		let td2 = '<td>' + items[i].email + '</td>';
+		let td3 = '<td>' + items[i].password + '</td>';
+		let td4 = '<td><button class="btn btn-danger btn-sm" index="' + i + '">x</button></td>';
+		$("#info").append('<tr>' + th + td1 + td2 + td3 + td4 + '</tr>');
+	}
+	// 削除ボタンにクリックイベント
+	$("#info tr button").click(function(){
+		let index = $(this).attr("index");
+		items.splice(index, 1);
+		saveData();// データのセーブ
+		showData();// データを表示
+	});
 }
