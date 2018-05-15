3d4fe736e6c4f3c38392a53d810e5098a37578a8
Shinichi_Ozaki <sdkfz181tiger@gmail.com>
2018-05-15 21:22:57 +0900

    フルサイズ

--- Video-js/js/custom.js
+++ Video-js/js/custom.js
@@ -1,2 +1,17 @@
 console.log("custom.js");
 
+// Ready
+$(document).ready(function(){
+	console.log("ready");
+
+	// Fullsize
+	$("#example_video_1").css({"width": "100%", "height": "100%"});
+
+	// Search
+	// $("#myBtn").click(function(){
+	// 	console.log("submit!!");
+	// 	let api  = $("#myApi").val();
+	// 	let city = $("#myCity").val();
+	// 	loadURL(api, city);
+	// });
+});
