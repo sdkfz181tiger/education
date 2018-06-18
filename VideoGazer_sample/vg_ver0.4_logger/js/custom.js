f4bf2dbda141057b86facc7fe39864c3603f258f
Shimeji Ozaki <sdkfz181tiger@gmail.com>
2018-06-18 20:56:22 +0900

    webgazerだけ

---
+++ VideoGazer_sample/vg_ver0.4_logger/js/custom.js
@@ -0,0 +1,206 @@
+console.log("custom.js");
+
+const DATA_KEY = "my_data";
+
+let player;
+let canvas;
+let jsonObj;
+
+// Ready
+$(document).ready(function(){
+	console.log("ready");
+
+	// Video.js
+	player = videojs("my_video");
+
+	player.on("ready", ()=>{
+		console.log("player:ready[]");
+
+		console.log(player);
+	});
+
+	player.on("play", ()=>{
+		console.log("player:play[" + checkLoaderMode() + "]");
+		if(checkLoaderMode() == false){
+			startLogger();
+		}else{
+			startLoader();
+		}
+	});
+
+	player.on("ended", ()=>{
+		console.log("player:ended[" + checkLoaderMode() + "]");
+		if(checkLoaderMode() == false){
+			stopLogger();
+		}else{
+			stopLoader();
+		}
+	});
+
+	// Canvas
+	canvas = document.getElementById("my_canvas");
+
+	// SelectBox
+	prepareSelectBox();
+});
+
+//==========
+// Logger
+function startLogger(){
+	console.log("startLogger");
+
+	// JsonObject
+	jsonObj = {};
+	jsonObj.data = [];
+
+	// Click(Enable)
+	$("video").on("click", (e)=>{
+		console.log("click");
+		// Data
+		let obj = {};
+		obj.pX = Math.floor(e.clientX / player.width() * 100.0);
+		obj.pY = Math.floor(e.clientY / player.height() * 100.0);
+		obj.cT = player.currentTime();
+		obj.pW = player.width() / 100.0;
+		obj.pH = obj.pW;
+		jsonObj.data.push(obj);
+	});
+
+	// Update
+	startUpdate();
+}
+
+function stopLogger(){
+	console.log("stopLogger");
+
+	// Click(Disable)
+	$("video").off("click");
+
+	// Save
+	saveLocalStorage(jsonObj);
+
+	// SelectBox
+	prepareSelectBox();
+}
+
+//==========
+// Loader
+function startLoader(){
+	console.log("startLoader");
+
+	// SelectBox
+	let val = $("#my_select").val();
+
+	// Load
+	jsonObj = loadLocalStorage(val);
+	console.log("date:" + jsonObj.date);
+	console.log("src:" + jsonObj.src);
+
+	// Update
+	startUpdate();
+}
+
+function stopLoader(){
+	console.log("stopLoader");
+}
+
+//==========
+// Canvas and Draw
+function startUpdate(){
+	setTimeout(()=>{
+		drawSprites();
+		if(player.ended() == false) startUpdate();
+	}, 200);
+}
+
+function drawSprites(){
+	//console.log("drawSprites");
+	let context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	for(let i=jsonObj.data.length-1; 0<=i; i--){
+		let obj   = jsonObj.data[i];
+		let objX  = canvas.width  * obj.pX / 100.0 - obj.pW * 0.5;
+		let objY  = canvas.height * obj.pY / 100.0 - obj.pH * 0.5;
+		let delay = player.currentTime() - obj.cT;
+		if(3.0 < delay) break;
+		if(delay < 0) continue;
+		context.fillRect(objX, objY, obj.pW, obj.pH);
+		if(0 < i){
+			let prev  = jsonObj.data[i-1];
+			let prevX = canvas.width * prev.pX / 100.0;
+			let prevY = canvas.height * prev.pY / 100.0;
+			context.beginPath();
+			context.moveTo(prevX, prevY);
+			context.lineTo(objX+obj.pW*0.5, objY+obj.pH*0.5);
+			context.stroke();
+		}
+	}
+}
+
+//==========
+// Utility
+function saveLocalStorage(obj){
+	// Time
+	let date = new Date();
+	let dFormat = new DateFormat("yyyy_MM_dd HH:mm:ss");
+	obj.date = dFormat.format(date);
+	// Src
+	obj.src = player.src();
+	// LocalStorage
+	let item = localStorage.getItem(DATA_KEY);
+	let storage = {};
+	storage.all = [];
+	if(item != null){
+		storage = JSON.parse(item);
+	}
+	storage.all.push(obj);
+	let str = JSON.stringify(storage);
+	localStorage.setItem(DATA_KEY, str);
+}
+
+function loadLocalStorage(index){
+	// LocalStorage
+	let item = localStorage.getItem(DATA_KEY);
+	let storage = {};
+	storage.all = [];
+	if(item != null){
+		storage = JSON.parse(item);
+	}
+	if(index == null || index < 0 || storage.all.length <= index){
+		return storage.all[storage.all.length - 1];
+	}
+	return storage.all[index];
+}
+
+function prepareSelectBox(){
+	console.log("prepareSelectBox");
+
+	// LocalStorage
+	let item = localStorage.getItem(DATA_KEY);
+	let storage = {};
+	storage.all = [];
+	if(item != null){
+		storage = JSON.parse(item);
+	}
+	// jQuery UI(SelectBox)
+	$("#my_select").selectmenu({
+		change: function(event, data){
+			// TODO:
+			console.log("change!!");
+		}
+	});
+	$("#my_select").empty();
+	for(let i=storage.all.length-1; 0<=i; i--){
+		let option = $("<option>").val(i).text(storage.all[i].date);
+		if(i == storage.all.length-1) option.prop("selected", true);
+		$("#my_select").append(option);
+	}
+	$("#my_select").selectmenu("refresh");
+	// jQuery UI(Checkbox)
+	$("#my_checkbox").checkboxradio();
+}
+
+function checkLoaderMode(){
+	return $("#my_checkbox").prop("checked");
+}
---
+++ VideoGazer_sample/vg_ver0.4_webgazer/libs/p5js/sketch.js
@@ -0,0 +1,146 @@
+console.log("Hello p5.js!!");
+
+const DISP_W     = 480;
+const DISP_H     = 320;
+const BALL_W     = 5;
+const BALL_H     = 5;
+const BALL_SPEED = 5;
+const PADDLE_W   = 50;
+const PADDLE_H   = 5;
+const BLOCK_W    = 5;
+const BLOCK_H    = 5;
+
+const FONT_SIZE  = 16;
+
+var ball, pCenter
+var wTop, wBottom, wLeft, wRight;
+var blocks;
+var score;
+var sScore;
+
+var img;
+
+function preload(){
+	console.log("preload");
+
+	// Font
+	var font = loadFont("assets/misaki_gothic.ttf");
+	textSize(FONT_SIZE);
+	textFont(font);
+
+	// Sound
+	sScore = loadSound("assets/s_score.mp3");
+
+	// Image
+	img = loadImage("assets/t_daruma.png");
+}
+
+function setup(){
+	console.log("setup");
+	createCanvas(DISP_W, DISP_H);
+	frameRate(32);
+	background(0);
+	fill(255, 255, 255);
+	noStroke();
+
+	// Ball
+	ball = createSprite(DISP_W*0.5, DISP_H-80, BALL_W, BALL_H);
+	ball.shapeColor = color(255, 255, 255);
+	ball.setSpeed(BALL_SPEED, 90);
+
+	// Paddle(Center)
+	pCenter = createSprite(DISP_W*0.5, DISP_H-30, PADDLE_W, PADDLE_H);
+	pCenter.shapeColor = color(255, 255, 255);
+	pCenter.immovable = true;
+
+	// Wall
+	wTop = createSprite(DISP_W*0.5, 0, DISP_W, 5);
+	wTop.shapeColor = color(200, 200, 200);
+	wTop.immovable = true;
+
+	wBottom = createSprite(DISP_W*0.5, DISP_H, DISP_W, 5);
+	wBottom.shapeColor = color(200, 200, 200);
+	wBottom.immovable = true;
+
+	wLeft = createSprite(0, DISP_H*0.5, 5, DISP_W);
+	wLeft.shapeColor = color(200, 200, 200);
+	wLeft.immovable = true;
+
+	wRight = createSprite(DISP_W, DISP_H*0.5, 5, DISP_H);
+	wRight.shapeColor = color(200, 200, 200);
+	wRight.immovable = true;
+
+	// Score
+	score = 0;
+
+	// Image
+	var colors = new Array();
+	for(var y=0; y<32; y++){
+		var lines = new Array();
+		for(var x=0; x<32; x++){
+			lines.push(img.get(x, y));
+		}
+		colors.push(lines);
+	}
+
+	// Blocks
+	blocks = new Array();
+	var rMax = 32;
+	var cMax = 32;
+	var bX = DISP_W*0.5 - BLOCK_W*(cMax-1)*0.5;
+	var bY = 50;
+	for(var r=0; r<rMax; r++){
+		for(var c=0; c<cMax; c++){
+			if(colors[r][c][3] != 0){
+				var block = createSprite(
+					BLOCK_W*c + bX, BLOCK_H*r + bY, BLOCK_W, BLOCK_H);
+				block.shapeColor = colors[r][c];
+				block.immovable = true;
+				blocks.push(block);
+			}
+		}
+	}
+}
+
+function draw(){
+	console.log("draw");
+	background(200);
+
+	// Paddle
+	if(ball.bounce(pCenter)){
+		var swing = (ball.position.x - pCenter.position.x) / 2;
+		ball.setSpeed(BALL_SPEED, ball.getDirection() + swing);
+	}
+
+	if(0 < mouseX && mouseX < DISP_W){
+		pCenter.position.x  = mouseX;
+		pCenter.position.x = mouseX;
+	}
+
+	// Wall
+	ball.bounce(wTop);
+	ball.bounce(wBottom);
+	ball.bounce(wLeft);
+	ball.bounce(wRight);
+
+	// Blocks
+	for(var i=0; i<blocks.length; i++){
+		if(ball.bounce(blocks[i])){
+			blocks[i].position.x = -100;
+			blocks[i].position.y = -100;
+			score++;
+			playSound(sScore);
+		}
+	}
+
+	// Text
+	textAlign(LEFT);
+	text("SCORE:"+score, 5, 20);
+
+	drawSprites();
+}
+
+function playSound(sound){
+	if(sound.isPlaying()) sound.stop();
+	sound.play();
+}
