d50669ada3be840709bac80e84f53ad6595bf473
Shimeji Ozaki <sdkfz181tiger@gmail.com>
2018-07-25 23:43:46 +0900

    教材整理中

--- p5-js_edu/HandsOn_Asteroids/Asteroids_vol0/js/sketch.js
+++ p5-js_edu/HandsOn_Asteroids/Asteroids_vol0/js/sketch.js
@@ -7,11 +7,13 @@ const DISP_W = 480;
 const DISP_H = 320;
 
 const PLAYER_SPEED    = 2;
-const PLAYER_FRICTION = 0.95;
-const BULLET_SPEED    = 5;
+const PLAYER_FRICTION = 0.98;
+const BULLET_SPEED    = 2;
 const BULLET_FRICTION = 1.0;
 
-const DEBUG  = true;
+const GENERATE_INTERVAL = 1000 * 2;
+
+const DEBUG = true;
 
 const images = [
 	"images/tanuki.png",
@@ -60,43 +62,59 @@ function setup(){
 	// Player
 	player = createPlayer(240, 160, "images/tanuki.png");
 
-	// Asteroids
-	let asteroid = createAsteroid(240, 160, "images/ume.png");
-	asteroid.setSpeed(1, 45);
-	asteroids.push(asteroid);
+	// Attack
+	generateAsteroid();
 }
 
 function draw(){
-	//console.log("draw");
 	background(0, 0, 0);
+
+	// Player
+	if(player.position.x < 0) player.position.x = width;
+	if(width < player.position.x) player.position.x = 0;
+	if(player.position.y < 0) player.position.y = height;
+	if(height < player.position.y) player.position.y = 0;
+
+	// Collide
+	for(let a=asteroids.length-1; 0<=a; a--){
+		// Asteroid x Player
+		asteroids[a].bounce(player);
+		// Asteroid x Bullet
+		for(let b=bullets.length-1; 0<=b; b--){
+			if(asteroids[a].bounce(bullets[b])){
+				bullets[b].position.x = -100;
+				bullets[b].position.y = -100;
+			}
+		}
+	}
+
+	// Asteroids
+	cleanOutside(asteroids);
+	cleanOutside(bullets);
 
 	drawSprites();
 }
 
 function keyPressed(){
-	console.log("keyPressed:", keyCode);
 
 	// Up
 	if(keyCode == 38){
 		player.setSpeed(PLAYER_SPEED, player.rotation-90);
 	}
-
 	// Left
 	if(keyCode == 37){
 		player.rotationSpeed = -5;
 	}
-
 	// Right
 	if(keyCode == 39){
 		player.rotationSpeed = +5;
 	}
-
 	// Shot(Z)
 	if(keyCode == 90){
 		let x = player.position.x;
 		let y = player.position.y;
 		let rotation = player.rotation-90;
-		let bullet = createBullet(x, y, rotation);
+		let bullet = createBullet(x, y);
 		bullet.setSpeed(BULLET_SPEED, rotation);
 		bullets.push(bullet);
 	}
@@ -108,6 +126,19 @@ function keyReleased(){
 	}
 }
 
+function generateAsteroid(){
+	console.log("generateAsteroid!!");
+
+	let asteroid = createAsteroid(1, 5, null);
+	asteroids.push(asteroid);
+
+	// Timeout
+	setTimeout(generateAsteroid, GENERATE_INTERVAL);
+}
+
+//==========
+// Utility
+
 function createPlayer(x, y, path){
 	let spr = createSprite(x, y, 16, 16);
 	spr.addImage(assets[path]);
@@ -116,17 +147,59 @@ function createPlayer(x, y, path){
 	return spr;
 }
 
-function createAsteroid(x, y, path){
+function createAsteroid(min, max, path){
+
+	// Asteroids
+	let x = width / 2;
+	let y = height / 2;
+	let speed = random(min, max);
+	let rotation = random(0, 360);
+
+	if(315 <= rotation || rotation < 45){
+		x = 0; y = random(0, height);
+	}
+	if(45 <= rotation && rotation < 135){
+		x = random(0, width); y = 0;
+	}
+	if(135 <= rotation && rotation < 225){
+		x = width; y = random(0, height);
+	}
+	if(225 <= rotation && rotation < 315){
+		x = random(0, width); y = height;
+	}
+
 	let spr = createSprite(x, y, 16, 16);
-	spr.addImage(assets[path]);
+	if(path != null){
+		spr.addImage(assets[path]);
+	}else{
+		spr.shapeColor = color(255, 255, 255);
+	}
+	spr.setSpeed(speed, rotation);
 	spr.debug = DEBUG;
 	return spr;
 }
 
-function createBullet(x, y, rotation){
+function createBullet(x, y){
 	let spr = createSprite(x, y, 4, 4);
 	spr.shapeColor = color(255, 255, 255);
 	spr.friction = BULLET_FRICTION;
 	spr.debug = DEBUG;
 	return spr;
+}
+
+function cleanOutside(sprites){
+	for(let i=sprites.length-1; 0<=i; i--){
+		if(isOutside(sprites[i])){
+			sprites[i].remove();
+			sprites.splice(i, 1);
+		}
+	}
+}
+
+function isOutside(sprite){
+	if(sprite.position.x < 0) return true;
+	if(width < sprite.position.x) return true;
+	if(sprite.position.y < 0) return true;
+	if(width < sprite.position.y) return true;
+	return false;
 }
