ab208c7f95b5fd6d6ded072e17663edde911335c
Shimeji Ozaki <sdkfz181tiger@gmail.com>
2018-10-06 11:42:51 +0900

    VR用コンテンツ用意

---
+++ Three-js_VR/04_HelloControl/js/utility.js
@@ -0,0 +1,134 @@
+console.log("utility.js!!");
+
+const DEG_TO_RAD = Math.PI / 180;
+const RAD_TO_DEG = 180 / Math.PI;
+
+// Three.js
+class ThreeManager{
+
+	constructor(mode=false, 
+		x=0, y=0, z=10, rX=0, rY=0, rZ=0){
+		console.log("ThreeManager");
+
+		// PC or VR
+		// false: PC mode(default)
+		// true:  VR mode
+		this._mode = mode;
+
+		// Polyfill(for VR)
+		this._polyfill = new WebVRPolyfill();
+
+		// Scene
+		this._scene = new THREE.Scene();
+
+		// Stats
+		this._stats = new Stats();
+		this._stats.setMode(0);
+		this._stats.domElement.style.position = "absolute";
+		this._stats.domElement.style.left = "0px";
+		this._stats.domElement.style.top  = "0px";
+		document.body.appendChild(this._stats.domElement);
+
+		// Axes
+		this._axes = new THREE.AxisHelper(5);
+		this._scene.add(this._axes);
+
+		// Camera
+		this._camera = new THREE.PerspectiveCamera(
+			75, window.innerWidth/window.innerHeight, 0.1, 1000);
+
+		// Container(for VR)
+		this._cameraContainer = new THREE.Object3D();
+		this._cameraContainer.add(this._camera);
+		this._scene.add(this._cameraContainer);
+
+		// PC or VR
+		if(this._mode == false){
+			// Camera
+			this._camera.position.set(x, y, z);// PCでポジションを移動させる場合
+			this._cameraContainer.rotation.set(rX*DEG_TO_RAD, rY*DEG_TO_RAD, rZ*DEG_TO_RAD);
+			// Controls
+			this._controls = new THREE.TrackballControls(this._camera);// Cameraのみ対応
+			this._controls.target.set(0, 0, 0);
+		}else{
+			// CameraContainer
+			this._cameraContainer.position.set(x, y, z);// VRでポジションを移動させる場合
+			this._cameraContainer.rotation.set(rX*DEG_TO_RAD, rY*DEG_TO_RAD, rZ*DEG_TO_RAD);
+			// Button
+			document.body.appendChild(WEBVR.createButton(this._renderer));
+		}
+
+		// Light
+		this._directionalLight = new THREE.DirectionalLight(0xffffff);
+		this._directionalLight.position.set(0, 0.7, 0.7);
+		this._scene.add(this._directionalLight);
+
+		// Renderer
+		this._renderer = new THREE.WebGLRenderer({antialias: true});
+		this._renderer.setSize(window.innerWidth, window.innerHeight);
+		this._renderer.setClearColor(0x333333);
+		this._renderer.setPixelRatio(window.devicePixelRatio);
+		this._renderer.vr.enabled = this._mode;// Important(for VR)
+		document.body.appendChild(this._renderer.domElement);
+
+		// Promises
+		this._promises = [];
+	}
+
+	animate(){
+
+		// Stats
+		this._stats.update();
+
+		// Controls(for PC)
+		this._controls.update();
+
+		// Render
+		this._renderer.render(this._scene, this._camera);
+	}
+
+	//==========
+	// Promise
+	startPromise(assets, onSuccess, onError){
+
+		for(let i=0; i<assets.data.length; i++){
+			console.log(assets.data[i]);
+			let data = assets.data[i];
+			this._promises.push(
+				this.asyncPromise(data.path, data.mtl, data.obj));
+		}
+		Promise.all(this._promises).then(onSuccess, onError);
+	}
+
+	// Promise
+	asyncPromise(path, mtl, obj){
+		return new Promise((resolve, reject)=>{
+			// MTLLoader
+			let mtlLoader = new THREE.MTLLoader();
+			mtlLoader.setPath(path);
+			mtlLoader.load(mtl, (materials)=>{
+				console.log("onLoaded:" + mtl);
+				materials.preload();
+				// OBJLoader
+				let objLoader = new THREE.OBJLoader();
+				objLoader.setPath(path);
+				objLoader.setMaterials(materials);
+				objLoader.load(obj, (meshes)=>{
+					meshes.children.forEach((mesh)=>{
+						mesh.geometry.computeFaceNormals();
+						mesh.geometry.computeVertexNormals();
+					});
+					meshes.scale.set(1, 1, 1);
+					meshes.rotation.set(0, Math.PI, 0);
+					meshes.position.set(0, 0, 0);
+					resolve(meshes);// Resolve
+				});
+			}, (progress)=>{
+				console.log("onProgress");
+			}, (error)=>{
+				console.log("onError:" + error);
+				reject(error);// Reject
+			});
+		});
+	}
+}
---
+++ Three-js_VR/05_HelloLines/js/main.js
@@ -0,0 +1,75 @@
+//==========
+// Three.js
+// -> https://threejs.org/
+
+console.log("Hello Three.js!!");
+
+// Data
+const assets = {data:[
+	{path:"./models/", mtl:"inv01.mtl", obj:"inv01.obj"},
+	{path:"./models/", mtl:"inv02.mtl", obj:"inv02.obj"},
+	{path:"./models/", mtl:"inv03.mtl", obj:"inv03.obj"},
+	{path:"./models/", mtl:"inv04.mtl", obj:"inv04.obj"},
+]};
+
+window.onload = function(){
+	console.log("OnLoad");
+
+	// ThreeManager
+	// VR mode: true/false
+	// Camera position: x, y, z
+	// Camera angle: rX, rY, rZ
+	let tm = new ThreeManager(false, 0, 10, 50, 0, 0, 0);
+	tm._renderer.setAnimationLoop(animate);
+	tm.startPromise(assets, 
+		(results)=>{onReady(results);},
+		(error)=>{onError(error);});
+
+	// Invaders
+	let invaders = [];
+
+	// Ready
+	function onReady(meshes){
+		console.log("You are ready to start the game!!");
+
+		let rows = meshes.length;
+		let cols = 8;
+		let padding = 6;
+		let startX = -padding * (cols-1) * 0.5;
+		let startY = 0.0;
+		for(let r=0; r<rows; r++){
+			for(let c=0; c<cols; c++){
+				let x = startX + padding * c;
+				let y = startY + padding * r;
+				let clone = meshes[r].clone();
+				clone.scale.set(0.4, 0.4, 0.4);
+				clone.rotation.set(0, Math.PI, 0);
+				clone.position.set(x, y, 0);
+				invaders.push(clone);
+				tm._scene.add(clone);
+			}
+		}
+	}
+
+	// Error
+	function onError(error){
+		console.log("Something went wrong...");
+		console.log(error);
+	}
+
+	// Animate
+	function animate(){
+		//console.log("Animate");
+
+		// Invaders
+		for(invader of invaders){
+			//invader.rotation.x += 0.005;
+			invader.rotation.y += 0.015;
+			//invader.rotation.z += 0.005;
+		}
+
+		// Manager
+		tm.animate();
+	};
+}
+
