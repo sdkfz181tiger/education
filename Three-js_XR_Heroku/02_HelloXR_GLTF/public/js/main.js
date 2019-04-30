ed2223cba6de7792e4df200da5101059109230ba
ShimejiOzaki <sdkfz181tiger@gmail.com>
2019-04-30 19:36:48 +0900

    Tap to gltf

---
+++ Three-js_XR_Heroku/02_HelloXR_GLTF/public/js/main.js
@@ -0,0 +1,77 @@
+//==========
+// Three.js
+// -> https://threejs.org/
+
+console.log("Hello Three.js!!");
+
+const ACT_STATES = ["Idle", "Walking", "Running", "Dance", "Death", "Sitting", "Standing"];
+const ACT_EMOTES = ["Jump", "Yes", "No", "Wave", "Punch", "ThumbsUp"];
+
+// Data
+const modelsGLTF = {data:[
+	{dir:"./models/gltf/RobotExpressive/", glb:"RobotExpressive.glb"}
+]};
+
+const sounds = {data:[
+	{dir:"./sounds/", mp3:"test_1.mp3"},
+	{dir:"./sounds/", mp3:"test_2.mp3"},
+	{dir:"./sounds/", mp3:"test_3.mp3"},
+	{dir:"./sounds/", mp3:"test_4.mp3"},
+]};
+
+const fonts = {data:[
+	{dir:"./fonts/", font:"MisakiGothic_Regular.json"},
+	{dir:"./fonts/", font:"MisakiMincho_Regular.json"},
+]};
+
+window.onload = function(){
+	console.log("OnLoad");
+
+	// ThreeManager
+	let tm = null;
+
+	// XRUtils
+	THREE.WebXRUtils.getDisplays().then((displays)=>{
+		// ThreeManager
+		tm = new ThreeManager(displays);
+
+		// Raycaster
+		tm.setRaycasterListener((intersects)=>{
+
+			for(let target of intersects){
+				console.log("distance:" + target.distance + "_" + target.object.name);
+				// Reticle
+				if(target.object.name == NAME_RETICLE){
+					// Cube
+					console.log("Catch!!");
+				}
+			}
+
+			// Model
+			let model = gltfLoader.findModels("RobotExpressive.glb");
+			let gltf = model.scene;
+			gltf.scale.set(0.05, 0.05, 0.05);
+			tm.moveToReticle(gltf);// Add to group!!
+		});
+
+		let gltfLoader = new GLTFLoader();
+		gltfLoader.loadModels(modelsGLTF, onReadyModels, onError);
+
+		let fontLoader = new FontLoader();
+		fontLoader.loadFonts(fonts,   onReadyFonts,  onError);
+
+		function onReadyModels(){
+			console.log("onReadyModels");
+		}
+
+		function onReadyFonts(){
+			console.log("onReadyFonts");
+		}
+
+		// Error
+		function onError(error){
+			console.log("Something went wrong...");
+			console.log(error);
+		}
+	});
+}
---
+++ Three-js_XR_Heroku/02_HelloXR_GLTF/public/js/utility.js
@@ -0,0 +1,243 @@
+console.log("utility.js!!");
+
+const NAME_RETICLE = "reticle";
+const NAME_CUBE    = "cube";
+
+const DEG_TO_RAD = Math.PI / 180;
+const RAD_TO_DEG = 180 / Math.PI;
+
+const options = {AR_AUTOSTART: true};
+
+// Three.js
+class ThreeManager{
+
+	constructor(displays){
+		console.log("ThreeManager");
+
+		// Clock
+		this._clock = new THREE.Clock();
+
+		// Container
+		let container = document.createElement("div");
+		document.body.appendChild(container);
+
+		// Scene, Camera, Renderer
+		this._scene = new THREE.Scene();
+		this._camera = new THREE.PerspectiveCamera();
+		this._scene.add(this._camera);
+		this._renderer = new THREE.WebGLRenderer({alpha: true});
+		this._renderer.autoClear = false;
+		container.appendChild(this._renderer.domElement);
+
+		// HemiLight
+		this._hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+		this._hemiLight.position.set(0, 30, 0);
+		this._hemiLight.color.setHSL(0.7, 0.9, 0.7);
+		this._hemiLight.groundColor.setHSL(1, 1, 1);
+		this._scene.add(this._hemiLight);
+		this._hemiLightHelper = new THREE.HemisphereLightHelper(this._hemiLight, 10);
+		this._scene.add(this._hemiLightHelper);
+
+		// Light
+		this._dirLight = new THREE.DirectionalLight(0xffffff);
+		this._dirLight.position.set(0, 30, 0);
+		this._scene.add(this._dirLight);
+		this._dirLightHelper = new THREE.DirectionalLightHelper(this._dirLight, 5);
+		this._scene.add(this._dirLightHelper);
+
+		// Stats
+		this._stats = new Stats();
+		this._stats.setMode(0);
+		this._stats.domElement.style.position = "absolute";
+		this._stats.domElement.style.left     = "0px";
+		this._stats.domElement.style.top      = "0px";
+		document.body.appendChild(this._stats.domElement);
+
+		// Axes
+		this._axes = new THREE.AxesHelper(0.4);
+		this._scene.add(this._axes);
+
+		// Group
+		this._group = new THREE.Group();
+		this._scene.add(this._group);
+
+		// Reticle
+		this._reticle = new THREE.Mesh(
+			new THREE.RingGeometry(0.04, 0.05, 36, 64),
+			new THREE.MeshBasicMaterial({color: "#DDFFDD"})
+		);
+		this._reticle.geometry.applyMatrix(
+			new THREE.Matrix4().makeRotationX(THREE.Math.degToRad(-90)));
+		this._reticle.visible = false;
+		this._reticle.name = NAME_RETICLE;
+		this._group.add(this._reticle);
+
+		// Window resize
+		window.addEventListener("resize", this.resetWindow, false);
+
+		// Raycaster
+		let mouseVector = new THREE.Vector3();
+		let raycaster = new THREE.Raycaster();
+		this._raycasterListener = null;
+
+		// Click
+		window.addEventListener("click", (e)=>{
+			event.preventDefault();
+			let x = (e.layerX/window.innerWidth)*2-1;
+			let y = - (e.layerY/window.innerHeight)*2+1;
+			mouseVector.set(x, y, 0.5);
+			raycaster.setFromCamera(mouseVector, this._camera);
+			let intersects = raycaster.intersectObject(this._scene, true);
+			if(this._raycasterListener != null && 0 < intersects.length){
+				this._raycasterListener(intersects);// Callback
+			}
+		}, false);
+
+		// WebXRManager
+		this._renderer.xr = new THREE.WebXRManager(options, displays, 
+			this._renderer, this._camera, this._scene, (frame)=>{
+				this.update(frame);
+			});
+		this._renderer.xr.addEventListener("sessionStarted", (data)=>{
+			console.log("onSessionStarted:" + data.session.realityType);
+			this.resetWindow();
+		});
+	}
+
+	resetWindow(){
+		// TODO: Cannot set property 'aspect' of undefined"
+		this._camera.aspect = window.innerWidth / window.innerHeight;
+		this._camera.updateProjectionMatrix();
+	}
+
+	setRaycasterListener(callback){
+		this._raycasterListener = callback;
+	}
+
+	moveToReticle(mesh){
+		console.log("moveToReticle");
+		if(this._reticle == null && this._reticle.visible == false) return;
+		// Light
+		this._hemiLight.position.set(
+			this._reticle.position.x, 30.0, this._reticle.position.z);
+		this._dirLight.position.set(
+			this._reticle.position.x, 30.0, this._reticle.position.z);
+		// Mesh
+		mesh.visible = true;
+		mesh.position.copy(this._reticle.position);
+		mesh.quaternion.copy(this._reticle.quaternion);
+		this._group.add(mesh);
+	}
+
+	update(frame){
+		// Stats
+		this._stats.update();
+
+		let hit = frame.hitTestNoAnchor(0.5, 0.5);
+		let model     = new THREE.Matrix4();
+		let tempPos   = new THREE.Vector3();
+		let tempQuat  = new THREE.Quaternion();
+		let tempScale = new THREE.Vector3();
+		if(hit && hit.length > 0){
+			this._reticle.visible = true;
+			model.fromArray(hit[0].modelMatrix);
+			model.decompose(tempPos, tempQuat, tempScale);
+			this._reticle.position.copy(tempPos);
+			this._reticle.quaternion.copy(tempQuat);
+		}
+	}
+}
+
+// GLTFLoader
+class GLTFLoader{
+
+	constructor(){
+		console.log("GLTFLoader");
+	}
+
+	loadModels(models, onSuccess, onError){
+		let promises = [];
+		for(let i=0; i<models.data.length; i++){
+			let data = models.data[i];
+			promises.push(
+				this.asyncModel(data.dir, data.glb));
+		}
+		Promise.all(promises).then((results)=>{
+			this._models = results;// Models
+			onSuccess();
+		}, (error)=>{
+			console.log(error);
+			onError(error);
+		});
+	}
+
+	findModels(glb){
+		for(let i=0; i<this._models.length; i++){
+			if(this._models[i].glb != glb) continue;
+			return this._models[i];
+		}
+		return null;
+	}
+
+	asyncModel(dir, glb){
+		return new Promise((resolve, reject)=>{
+			// GLTFLoader
+			let path = dir + glb;
+			let loader = new THREE.GLTFLoader();
+			loader.load(path, (model)=>{
+				model.dir = dir; model.glb = glb;
+				resolve(model);// Resolve
+			}, undefined, (error)=>{
+				console.log("onError:" + error);
+				reject(error);// Reject
+			});
+		});
+	}
+}
+
+class FontLoader{
+
+	constructor(){
+		console.log("FontLoader");
+	}
+
+	loadFonts(fonts, onSuccess, onError){
+		let promises = [];
+		for(let i=0; i<fonts.data.length; i++){
+			let data = fonts.data[i];
+			promises.push(
+				this.asyncFonts(data.dir, data.font));
+		}
+		Promise.all(promises).then((results)=>{
+			this._fonts = results;// Fonts
+			onSuccess();
+		}, (error)=>{
+			console.log(error);
+			onError();
+		});
+	}
+
+	findFonts(name){
+		for(let i=0; i<this._fonts.length; i++){
+			if(this._fonts[i].data.familyName != name) continue;
+			return this._fonts[i];
+		}
+		return null;
+	}
+
+	asyncFonts(dir, font){
+		return new Promise((resolve, reject)=>{
+			// FontLoader
+			let loader = new THREE.FontLoader();
+			let path = dir + font;
+			loader.load(path, (result)=>{
+				resolve(result);// Resolve
+			}, (progress)=>{
+				//console.log("onProgress");
+			}, (error)=>{
+				console.log("onError:" + error);
+				reject(error);// Reject
+			});
+		});
+	}
+}
