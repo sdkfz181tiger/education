d420575dad51e010cae9975b2e38a9e65a10c426
ShimejiOzaki <sdkfz181tiger@gmail.com>
2019-06-17 23:55:07 +0900

    それっぽく

--- p5-js_game/t-rex-runner_0.1/js/sketch.js
+++ p5-js_game/t-rex-runner_0.1/js/sketch.js
@@ -1,11 +1,24 @@
 console.log("Hello p5.js!!");
 
+const GROUND_W  = 1200;
 const GROUND_Y  = 280;
-const GRAVITY_Y = 3;
+const GRAVITY_Y = 4;
 const JUMP_Y    = -35;
+const SPEED_GRD = -10;
+const SPEED_CAC = -10;
+const SPEED_PTR = -14;
 
-let font = null;
-let tRex = null;
+const CACTUSES = [
+	"./assets/ca_a_1.png", "./assets/ca_a_2.png", "./assets/ca_a_3.png",
+	"./assets/ca_b_1.png", "./assets/ca_b_2.png", "./assets/ca_b_3.png"];
+const PTERANOS = ["./assets/p_fly_1.png", "./assets/p_fly_2.png"];
+
+let font  = null;
+let tRex  = null;
+let gSpr1 = null;
+let gSpr2 = null;
+let cacs  = null;
+let ptrs  = null;
 
 function preload(){
 	// Font
@@ -16,7 +29,82 @@ function setup(){
 	console.log("setup!!");
 	createCanvas(480, 320);
 	background(200);
-	frameRate(32);
+	frameRate(16);
+
+	// GameStart
+	gameStart();
+} 
+
+function draw(){
+	//console.log("draw!!");
+	background(200);
+	drawTRex();
+	drawGround();
+	drawCacs();
+	drawPtrs();
+	drawSprites();
+}
+
+function drawTRex(){
+	// Gravity
+	let bottomY = tRex.getBoundingBox().bottom();
+	if(tRex.velY < 0 || bottomY < GROUND_Y){
+		tRex.velY       += GRAVITY_Y;
+		tRex.position.y += tRex.velY;
+	}
+	bottomY = tRex.getBoundingBox().bottom();
+	if(GROUND_Y < bottomY){
+		tRex.velY = 0;
+		tRex.position.y = GROUND_Y - tRex.getBoundingBox().extents.y * 0.5;
+	}
+}
+
+function drawGround(){
+	// Ground
+	gSpr1.position.x += SPEED_GRD;
+	gSpr2.position.x += SPEED_GRD;
+	if(gSpr1.position.x < -GROUND_W*0.5){
+		let offsetX = gSpr2.getBoundingBox().right();
+		gSpr1.position.x = offsetX + GROUND_W*0.5;
+	}
+	if(gSpr2.position.x < -GROUND_W*0.5){
+		let offsetX = gSpr1.getBoundingBox().right();
+		gSpr2.position.x = offsetX + GROUND_W*0.5;
+	}
+}
+
+function drawCacs(){
+	// Cacs
+	for(let i=cacs.length-1; 0<=i; i--){
+		let cac = cacs[i] ;
+		cac.position.x += SPEED_CAC;
+		// Overlap
+		if(tRex.overlap(cac)) gameOver();
+		// Remove
+		if(cac.position.x < 0){
+			cacs.splice(i, 1);
+			cac.remove();
+		}
+	}
+}
+
+function drawPtrs(){
+	// Ptrs
+	for(let i=ptrs.length-1; 0<=i; i--){
+		let ptr = ptrs[i] ;
+		ptr.position.x += SPEED_PTR;
+		// Overlap
+		if(tRex.overlap(ptr)) gameOver();
+		// Remove
+		if(ptr.position.x < 0){
+			ptrs.splice(i, 1);
+			ptr.remove();
+		}
+	}
+}
+
+function gameStart(){
+	console.log("gameStart!!");
 
 	// T-rex
 	tRex = createSprite(100, GRAVITY_Y, 32, 32);
@@ -24,32 +112,71 @@ function setup(){
 	tRex.addAnimation("jump",  "./assets/t_jump_1.png",  "./assets/t_jump_2.png");
 	tRex.addAnimation("stoop", "./assets/t_stoop_1.png", "./assets/t_stoop_2.png");
 	tRex.addAnimation("dead",  "./assets/t_dead_1.png",  "./assets/t_dead_2.png");
+	tRex.velY = 0;// Velocity
 
-	// Animation
-	tRex.changeAnimation("run");
+	// Ground
+	gSpr1 = createSprite(0, GROUND_Y-5, 32, 32);
+	gSpr1.addImage(loadImage("./assets/g_ground.png"));
+	gSpr2 = createSprite(GROUND_W, GROUND_Y-5, 32, 32);
+	gSpr2.addImage(loadImage("./assets/g_ground.png"));
 
-	// Jump
-	tRex.jumpY = 0;
+	// Cacs
+	cacs = [];
+	ptrs = [];
+	tickEnemies();
 }
 
-function draw(){
-	console.log("draw!!");
-	background(200);
-	drawSprites();
+function gameOver(){
+	console.log("gameOver!!");
+	noLoop();
+}
 
-	tRex.jumpY      += GRAVITY_Y;
-	tRex.position.y += tRex.jumpY;
-	if(GROUND_Y < tRex.position.y){
-		tRex.jumpY = 0;
-		tRex.position.y = GROUND_Y;
-		tRex.changeAnimation("run");// Run
+function tickEnemies(){
+	console.log("tickEnemies!!");
+	let rdm = floor(random(0, 10));
+	if(rdm < 8){
+		// Cacs
+		let index = floor(random(0, CACTUSES.length));
+		let path = CACTUSES[index];
+		cacs.push(createCac(path, 480, 265));
+	}
+	if(rdm < 3){
+		// Ptrs
+		let x = 480;
+		let y = floor(random(50, 220));
+		ptrs.push(createPtr(x, y));
+	}
+	setTimeout(()=>{tickEnemies();}, 3000);
+}
+
+function createCac(path, x=480, y=160){
+	let cac = createSprite(x, y, 32, 32);
+	cac.addImage(loadImage(path));
+	return cac;
+}
+
+function createPtr(x, y){
+	let ptr = createSprite(x, y, 32, 32);
+	ptr.addAnimation("fly", PTERANOS[0], PTERANOS[1]);
+	return ptr;
+}
+
+function keyPressed(){
+	console.log("keyPressed!!");
+
+	if(keyCode == 32){// Space
+		if(tRex.velY != 0) return;
+		tRex.velY = JUMP_Y;
+		tRex.changeAnimation("jump");// Jump
+	}
+
+	if(keyCode == 224){// Command
+		tRex.changeAnimation("stoop");// Stoop
 	}
 }
 
-function mousePressed(){
-	console.log("mousePressed!!");
+function keyReleased(){
+	console.log("keyReleased!!");
 
-	if(tRex.jumpY != 0) return;
-	tRex.jumpY = JUMP_Y;
-	tRex.changeAnimation("jump");// Jump
+	tRex.changeAnimation("run");// Run
 }
