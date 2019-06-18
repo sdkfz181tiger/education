2f94ecb9c7a08cef47a6b2a8bbd64356cf06e293
ShimejiOzaki <sdkfz181tiger@gmail.com>
2019-06-18 14:30:06 +0900

    改善

--- p5-js_game/t-rex-runner_0.1/js/sketch.js
+++ p5-js_game/t-rex-runner_0.1/js/sketch.js
@@ -1,24 +1,31 @@
 console.log("Hello p5.js!!");
 
-const GROUND_W  = 1200;
-const GROUND_Y  = 280;
-const GRAVITY_Y = 4;
-const JUMP_Y    = -35;
-const SPEED_GRD = -10;
-const SPEED_CAC = -10;
-const SPEED_PTR = -14;
+const DISP_W     = 480;
+const DISP_H     = 320;
 
+const TICK_TIME  = 2000;
+
+const GROUND_W   = 1200;
+const GROUND_Y   = 280;
+const GRAVITY_Y  = 4;
+const JUMP_Y     = -35;
+const SPEED_BASE = -10;
+const SPEED_CAC  = -10;
+const SPEED_PTR  = -14;
+
+const CLOUDS   = ["./assets/g_cloud.png"];
 const CACTUSES = [
 	"./assets/ca_a_1.png", "./assets/ca_a_2.png", "./assets/ca_a_3.png",
 	"./assets/ca_b_1.png", "./assets/ca_b_2.png", "./assets/ca_b_3.png"];
 const PTERANOS = ["./assets/p_fly_1.png", "./assets/p_fly_2.png"];
 
-let font  = null;
-let tRex  = null;
-let gSpr1 = null;
-let gSpr2 = null;
-let cacs  = null;
-let ptrs  = null;
+let font   = null;
+let tRex   = null;
+let gSpr1  = null;
+let gSpr2  = null;
+let clouds = null;
+let cacs   = null;
+let ptrs   = null;
 
 function preload(){
 	// Font
@@ -27,8 +34,7 @@ function preload(){
 
 function setup(){
 	console.log("setup!!");
-	createCanvas(480, 320);
-	background(200);
+	createCanvas(DISP_W, DISP_H);
 	frameRate(16);
 
 	// GameStart
@@ -37,12 +43,40 @@ function setup(){
 
 function draw(){
 	//console.log("draw!!");
-	background(200);
-	drawTRex();
+	background(150);
 	drawGround();
+	drawClouds();
+	drawTRex();
 	drawCacs();
 	drawPtrs();
 	drawSprites();
+}
+
+function drawGround(){
+	// Ground
+	gSpr1.position.x += SPEED_BASE;
+	gSpr2.position.x += SPEED_BASE;
+	if(gSpr1.position.x < -GROUND_W*0.5){
+		let offsetX = gSpr2.getBoundingBox().right();
+		gSpr1.position.x = offsetX + GROUND_W*0.5;
+	}
+	if(gSpr2.position.x < -GROUND_W*0.5){
+		let offsetX = gSpr1.getBoundingBox().right();
+		gSpr2.position.x = offsetX + GROUND_W*0.5;
+	}
+}
+
+function drawClouds(){
+	// Clouds
+	for(let i=clouds.length-1; 0<=i; i--){
+		let cloud = clouds[i] ;
+		cloud.position.x += SPEED_BASE;
+		// Remove
+		if(cloud.position.x < 0){ 
+			clouds.splice(i, 1);
+			cloud.remove();
+		}
+	}
 }
 
 function drawTRex(){
@@ -56,20 +90,6 @@ function drawTRex(){
 	if(GROUND_Y < bottomY){
 		tRex.velY = 0;
 		tRex.position.y = GROUND_Y - tRex.getBoundingBox().extents.y * 0.5;
-	}
-}
-
-function drawGround(){
-	// Ground
-	gSpr1.position.x += SPEED_GRD;
-	gSpr2.position.x += SPEED_GRD;
-	if(gSpr1.position.x < -GROUND_W*0.5){
-		let offsetX = gSpr2.getBoundingBox().right();
-		gSpr1.position.x = offsetX + GROUND_W*0.5;
-	}
-	if(gSpr2.position.x < -GROUND_W*0.5){
-		let offsetX = gSpr1.getBoundingBox().right();
-		gSpr2.position.x = offsetX + GROUND_W*0.5;
 	}
 }
 
@@ -120,9 +140,10 @@ function gameStart(){
 	gSpr2 = createSprite(GROUND_W, GROUND_Y-5, 32, 32);
 	gSpr2.addImage(loadImage("./assets/g_ground.png"));
 
-	// Cacs
-	cacs = [];
-	ptrs = [];
+	// Clouds, Cacs, Ptrs
+	clouds = [];
+	cacs   = [];
+	ptrs   = [];
 	tickEnemies();
 }
 
@@ -134,22 +155,39 @@ function gameOver(){
 function tickEnemies(){
 	console.log("tickEnemies!!");
 	let rdm = floor(random(0, 10));
+	for(let i=0; i<3; i++){
+		// Clouds
+		let index = floor(random(0, CLOUDS.length));
+		let path = CLOUDS[index];
+		clouds.push(createCloud(path));
+	}
+
+	// Cacs, Ptrs
 	if(rdm < 8){
 		// Cacs
 		let index = floor(random(0, CACTUSES.length));
 		let path = CACTUSES[index];
-		cacs.push(createCac(path, 480, 265));
+		cacs.push(createCac(path, DISP_W, 265));
 	}
+
 	if(rdm < 3){
 		// Ptrs
-		let x = 480;
+		let x = DISP_W;
 		let y = floor(random(50, 220));
 		ptrs.push(createPtr(x, y));
 	}
-	setTimeout(()=>{tickEnemies();}, 3000);
+	setTimeout(()=>{tickEnemies();}, TICK_TIME);
 }
 
-function createCac(path, x=480, y=160){
+function createCloud(path){
+	let x = DISP_W + random(0, 200);
+	let y = floor(random(0, 160));
+	let cloud = createSprite(x, y, 32, 32);
+	cloud.addImage(loadImage(path));
+	return cloud;
+}
+
+function createCac(path, x=DISP_W, y=160){
 	let cac = createSprite(x, y, 32, 32);
 	cac.addImage(loadImage(path));
 	return cac;
@@ -162,7 +200,7 @@ function createPtr(x, y){
 }
 
 function keyPressed(){
-	console.log("keyPressed!!");
+	//console.log("keyPressed!!");
 
 	if(keyCode == 32){// Space
 		if(tRex.velY != 0) return;
@@ -176,7 +214,7 @@ function keyPressed(){
 }
 
 function keyReleased(){
-	console.log("keyReleased!!");
+	//console.log("keyReleased!!");
 
 	tRex.changeAnimation("run");// Run
 }
