6558fcf17f8bedb6cb77384308a6281a9b9ef7e4
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-07-14 14:14:07 +0900

    Howler.jsに決定

--- Three-js_Meikoudai/04_SoundGame/js/main.js
+++ Three-js_Meikoudai/04_SoundGame/js/main.js
@@ -4,12 +4,6 @@
 
 console.log("Hello Three.js!!");
 
-// Steps
-const STEP_FORWARD = "forward";
-const STEP_BACK    = "back";
-const STEP_LEFT    = "left";
-const STEP_RIGHT   = "right";
-
 // Data
 const models = {data:[
 	{dir:"./models/obj/", mtl:"city_1.mtl",        obj:"city_1.obj"},
@@ -40,8 +34,7 @@ const models = {data:[
 ]};
 
 const sounds = {data:[
-	{dir:"./sounds/", mp3:"step_ok.mp3"},
-	{dir:"./sounds/", mp3:"step_ng.mp3"}
+	{dir:"./sounds/", mp3:"effect_1.mp3"}
 ]};
 
 const fonts = {data:[
@@ -49,19 +42,47 @@ const fonts = {data:[
 	{dir:"./fonts/", font:"MisakiMincho_Regular.json"},
 ]};
 
+// Howler
+let howl        = null;
+let currentTime = 0.0;
+
+// ThreeManager
 let tm          = null;
 let rootGroup   = null;
 
+// Loader
 let objLoader   = null;
 let soundLoader = null;
 let fontLoader  = null;
 
-let player      = null;
-let actors      = null;
-
 window.onload = function(){
 	console.log("OnLoad");
+	readyHowler();// Ready
+}
 
+// Howler.js
+function readyHowler(){
+	// Howler
+	let src = "./sounds/bgm_1.mp3";
+	howl = new Howl({src: [src]});
+
+	howl.on("load", function(){
+		console.log("Howler:load");
+		howl.volume(1.0);// Volume 0.0 ~ 1.0
+		readyThreeJS();// Ready
+	});
+
+	howl.on("play", function(){
+		console.log("Howler:play");
+	});
+
+	howl.on("end", function(){
+		console.log("Howler:end");
+	});
+}
+
+// Three.js
+function readyThreeJS(){
 	// ThreeManager
 	// 	Camera position(PC): pcX, pcY, pcZ
 	tm = new ThreeManager(0, 60, 90);
@@ -90,24 +111,12 @@ window.onload = function(){
 	function onReadyModels(){
 		console.log("You are ready to use models!!");
 
-		// Player
-		player = new Player(0, 0, +2, "tanuki_run_1.obj");
-
 		// Camera
 		let cContainer = tm.getCameraContainer();
 
 		// Skybox
 		let skybox = tm.createSkybox("./textures/skybox_space.png", 6, 300);
 		rootGroup.add(skybox);
-
-		// Tile
-		let road1 = new MyActor(-4, +0, -4, "road_1.obj");
-		road1.getPosition().y = -0.5;
-		let road2 = new MyActor(+0, +0, -4, "road_1.obj");
-		road2.getPosition().y = -0.5;
-		let road3 = new MyActor(+4, +0, -4, "road_1.obj");
-		road3.getPosition().y = -0.5;
-
 		
 		// Cube
 		let geometry = new THREE.BoxGeometry(3, 3, 3);
@@ -119,66 +128,15 @@ window.onload = function(){
 
 		let ctlForward = new THREE.Mesh(geometry, material);
 		ctlForward.position.set(0, 0, -4);
-		ctlForward.name = STEP_FORWARD;
+		ctlForward.name = "hoge";
 		ctlGroup.add(ctlForward);
-
-		let ctlBack = new THREE.Mesh(geometry, material);
-		ctlBack.position.set(0, 0, +4);
-		ctlBack.name = STEP_BACK;
-		ctlGroup.add(ctlBack);
-
-		let ctlLeft = new THREE.Mesh(geometry, material);
-		ctlLeft.position.set(-4, 0, 0);
-		ctlLeft.name = STEP_LEFT;
-		ctlGroup.add(ctlLeft);
-
-		let ctlRight = new THREE.Mesh(geometry, material);
-		ctlRight.position.set(+4, 0, 0);
-		ctlRight.name = STEP_RIGHT;
-		ctlGroup.add(ctlRight);
 
 		// Raycaster
 		tm.setRaycasterListener((intersects)=>{
 			for(let target of intersects){
 				let name = target.object.name;
 				if(name === "") continue;
-
-				// Player
-				if(name == STEP_FORWARD){
-					let flg = true;
-					for(let actor of actors){
-						if(actor._boardFlg == true) continue;// Board or not
-						if(player.intersectsSensor(actor)) flg = false;// Sensor
-					}
-					player.stepOut(0.0, 2.5, -5.0, !flg);
-				}
-
-				if(name == STEP_BACK){
-					let flg = true;
-					for(let actor of actors){
-						if(actor._boardFlg == true) continue;// Board or not
-						if(player.containsPoint(actor, 0, 0, +1)) flg = false;
-					}
-					player.stepOut(0.0, 2.5, +5.0, !flg);
-				}
-
-				if(name == STEP_LEFT){
-					let flg = true;
-					for(let actor of actors){
-						if(actor._boardFlg == true) continue;// Board or not
-						if(player.containsPoint(actor, -1, 0, 0)) flg = false;
-					}
-					player.stepOut(-5.0, 2.5, 0.0, !flg);
-				}
-
-				if(name == STEP_RIGHT){
-					let flg = true;
-					for(let actor of actors){
-						if(actor._boardFlg == true) continue;// Board or not
-						if(player.containsPoint(actor, +1, 0, 0)) flg = false;
-					}
-					player.stepOut(+5.0, 2.5, 0.0, !flg);
-				}
+				console.log("name:" + name);
 			}
 		});
 
@@ -187,6 +145,9 @@ window.onload = function(){
 
 		// Animation
 		tm._renderer.setAnimationLoop(animate);
+
+		// Howler
+		howl.play();
 	}
 
 	function onReadySounds(){
@@ -195,7 +156,7 @@ window.onload = function(){
 
 	function onReadyFonts(){
 		console.log("You are ready to use fonts!!");
-		// Test
+		// Font
 		let font = fontLoader.findFonts("MisakiGothic");
 		let text = fontLoader.createText("How are you!?", font, 4, 0, 20, -50);
 		rootGroup.add(text);
@@ -210,6 +171,10 @@ window.onload = function(){
 	function animate(){
 		tm.update();   // Manager
 		ctlVR.update();// Controller
-		if(player != null) player.surfBoard();// Surfing
+		// Time
+		let cTime = howl.seek() || 0;
+		let tTime = howl.duration();
+		let per = Math.floor((cTime/tTime)*100);
+		console.log(per + "%");
 	};
 }
