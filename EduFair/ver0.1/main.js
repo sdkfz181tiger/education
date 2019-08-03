7cac8303b3dcfa86cbe26f045a4ab553f7d22cf7
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-08-04 00:06:44 +0900

    調整

--- EduFair/ver0.1/main.js
+++ EduFair/ver0.1/main.js
@@ -2,7 +2,7 @@
     教育フェア用教材
 */
 
-const TIME_LIMIT = 40;
+const TIME_LIMIT = 10;
 
 var assets = [
     "images/cf001/b_left.png",   "images/cf001/b_right.png",
@@ -12,9 +12,10 @@ var assets = [
     "images/cf001/pod.png",      "images/cf001/table.png",
     "images/cf001/cannon.png",   "images/cf001/codefriends.png",
     "images/cf001/medal.png",    "images/cf001/coin.png",
+    "images/cf001/gameclear.png", "images/cf001/gameover.png",
     "sounds/cf001/medal.mp3",     "sounds/cf001/coin.mp3",
-    "sounds/cf001/omg.mp3",
-    "sounds/cf001/bgm_game.mp3", "sounds/cf001/bgm_gameover.mp3",
+    "sounds/cf001/omg.mp3",       "sounds/cf001/bgm_game.mp3", 
+    "sounds/cf001/bgm_clear.mp3", "sounds/cf001/bgm_over.mp3",
 ];
 
 function gameStart(){
@@ -56,7 +57,7 @@ function gameStart(){
 
     var bgm = core.assets["sounds/cf001/bgm_game.mp3"];
     bgm.src.loop = true;
-    //bgm.play();
+    bgm.play();
 
     var ground = createBox(0, 350-32, 320*10, 64);
     ground.backgroundColor = "green";
@@ -88,7 +89,9 @@ function gameStart(){
     cannon.centerX = 110; cannon.centerY = 225;
     cannon.addEventListener(Event.TOUCH_START, ()=>{
         if(cannon.frame != 0) return;
-        cannon.frame = 1; fire();// Fire!!
+        cannon.frame = 1; 
+        startTimer();// Timer
+        fire();// Fire!!
     });
     area.addChild(cannon);
 
@@ -96,10 +99,17 @@ function gameStart(){
     bar.backgroundColor = "black";
     scene.addChild(bar);
 
+    // Score
     var score = 0;
     var sLabel = createLabel(5, 5, 160, "left");
     sLabel.text = "SCORE:" + score;
     scene.addChild(sLabel);
+
+    // Timer
+    var time = TIME_LIMIT;
+    var tLabel = createLabel(155, 5, 160, "right");
+    tLabel.text = "TIME:" + time;
+    scene.addChild(tLabel);
 
     function createTable(x, y, iW=8, iH=10, iPath="", tag="", s=10){
         var podL = new Sprite(29, 53);
@@ -124,184 +134,6 @@ function gameStart(){
         item.score = s;
         area.addChild(item);
     }
-
-    /*
-    //==========
-    // ここから
-    //==========
-
-    var ball = createBall(110, 130, 16, "images/warp_brown.png", 5);
-    area.addChild(ball);
-
-    function actionL(){
-        ball.addImpulse(-4.5, -6);
-    }
- 
-    function actionR(){
-        ball.addImpulse(+4.5, -6);
-        //ball.addImpulse(10, -24);
-    }
- 
-    function actionB(){
-        ball.addImpulse(0, -8);
-    }
-
-    //==========
-    // ここまで
-    //==========
-
-    var tS = createBox(110, 160, 45, 160, "images/ft_tunnel.png");
-    area.addChild(tS);
-
-    var tG = createBox(1580, 80, 45, 240, "images/ft_tunnel.png");
-    area.addChild(tG);
-    for(var i=0; i<30; i++){
-        var gG = createTarget(1400, 80, 16, "images/codefriends.png", 0, 100);
-        area.addChild(gG);
-    }
-
-    var ume1 = createTarget(-1580, 200, 16, "images/hat_ume.png", 0, -10000);
-    area.addChild(ume1);
-
-    for(var i=0; i<6; i++){
-        makeTns(300+200*i, 160, 160, "images/mk_coin.png", 10);
-    }
-
-    function makeTns(x, y, h, path, s){
-        var t = createBox(x, y, 45, h, "images/ft_tunnel.png");
-        area.addChild(t);
-        var g = createTarget(x, y-8, 16, path, 0, s);
-        area.addChild(g);
-    }
-
-    var jp1 = createJackpod(110, -300, "images/codefriends.png", 5);
-    area.addChild(jp1);
-
-    for(var i=0; i<5; i++){
-        var x = -1000 + 35 * i;
-        var y = 200 - 35 * i;
-        var jp = createJackpod(x, y, "images/hat_musu.png", 5);
-        area.addChild(jp);
-    }
-
-    function createJackpod(x, y, path, t=5){
-        var block = createBox(x, y, 32, 42, "images/bal_baloon.png");
-        block.addCollision(ball);
-        block.addEventListener(Event.COLLISION, ()=>{
-            block.frame = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, null];
-            block.addEventListener(Event.ANIMATION_END, ()=>{
-                for(var i=0; i<t; i++){
-                   var a = block.centerX;
-                   var b = block.y-32 - i * 32;
-                   var g = createTarget(a, b, 16, path, 0, 100);
-                   area.addChild(g);
-                }
-                block.destroy();
-            });
-        });
-        return block;
-    }
-
-    // 世界の果て
-    var hw1 = createBox(2000, -320*5, 64, 320*10, "images/mk_ground.png");
-    area.addChild(hw1);
-    var hw2 = createBox(2000, 320*2, 
-                        32*9, 32*2, "images/mk_ground.png");
-    area.addChild(hw2);
-    var hw3 = createBox(1800, 320*2-64, 
-                        32*9, 32, "images/mk_ground.png");
-    hw3.angle = 45;
-    area.addChild(hw3);
-
-    var hwlL = createLabel(1900, 320*2-100);
-    hwlL.text = "ざんねん";
-    hwlL.color = "red";
-    var hwlR = createLabel(2100, 320*2-100);
-    hwlR.text = "あたり";
-    hwlR.color = "green";
-    area.addChild(hwlR);
-    var hwL = createTarget(1900, 320*2-100, 
-                           16, "images/hat_ume.png", 0, -10000);
-    area.addChild(hwL);
-
-    var btnL = createSprite(160-80, 320, 62, 62, "images/b_left.png");
-    scene.addChild(btnL);
-    btnL.on(Event.TOUCH_START, ()=>{
-        ball.vx = 0; ball.vy = 0; actionL();
-        core.assets["sounds/jump.mp3"].clone().play();
-    });
-
-    var btnR = createSprite(160, 320, 62, 62, "images/b_right.png");
-    scene.addChild(btnR);
-    btnR.on(Event.TOUCH_START, ()=>{
-        ball.vx = 0; ball.vy = 0; actionR();
-        core.assets["sounds/jump.mp3"].clone().play();
-    });
-
-    var btnB = createSprite(160+80, 320, 62, 62, "images/b_blue.png");
-    scene.addChild(btnB);
-    btnB.on(Event.TOUCH_START, ()=>{
-        ball.vx = 0; ball.vy = 0; actionB();
-        core.assets["sounds/cat.mp3"].clone().play();
-    });
-
-    var bgm = core.assets["sounds/bgm_game.mp3"];
-    bgm.src.loop = true;
-    //bgm.play();
-
-    var ground = createBox(0, 320-32, 320*10, 64, "images/mk_ground.png");
-    area.addChild(ground);
-
-    area.setScrollRange(ball, 90, 90, 64, 90);
-
-    var bar = new Sprite(320, 30);
-    bar.backgroundColor = "black";
-    scene.addChild(bar);
-
-    // Score
-    var score = 0;
-    var sLabel = createLabel(5, 5, 160, "left");
-    sLabel.text = "SCORE:" + score;
-    scene.addChild(sLabel);
-
-    // Timer
-    var time = TIME_LIMIT;
-    var tLabel = createLabel(155, 5, 160, "right");
-    tLabel.text = "TIME:" + time;
-    scene.addChild(tLabel);
-    // Count down
-    scene.tl.delay(16);
-    scene.tl.then(()=>{
-        time--;
-        tLabel.text = "TIME:" + time;
-        if(time <= 0) gameOver();
-    });
-    scene.tl.loop();
-
-    function createBall(x, y, r, path, f=0){
-        var spr = new PhyCircleSprite(r, enchant.box2d.DYNAMIC_SPRITE);
-        spr.image = core.assets[path]; spr.frame = f;
-        spr.centerX = x; spr.centerY = y;
-        return spr;
-    }
-
-    function createTarget(x, y, r, path, f=0, s=10){
-        var spr = new PhyCircleSprite(r, enchant.box2d.DYNAMIC_SPRITE);
-        spr.image = core.assets[path]; spr.frame = f;
-        spr.centerX = x; spr.centerY = y;
-        spr.addCollision(ball);
-        spr.addEventListener(Event.COLLISION, ()=>{
-            var lbl = createLabel(spr.centerX, spr.y);
-            lbl.text = s; area.addChild(lbl);
-            if(s<0) lbl.color = "red";
-            lbl.tl.moveBy(0, -30, 16);
-            lbl.tl.then(()=>{lbl.remove();});
-            updateScore(s);
-            spr.destroy();
-        });
-        return spr;
-    }
-*/
 
     function createBox(x, y, w, h, path="", f=0, type=false){
         if(type){
@@ -350,11 +182,31 @@ function gameStart(){
         }
     }
 
+    function startTimer(){
+        // Count down
+        scene.tl.delay(16);
+        scene.tl.then(()=>{
+            time--;
+            tLabel.text = "TIME:" + time;
+            if(time <= 0) gameOver();
+        });
+        scene.tl.loop();
+    }
+
+    function gameClear(){
+        bgm.stop();
+        var spr = createSprite(160, 140, 230, 32, "images/cf001/gameclear.png");
+        scene.addChild(spr);
+        var snd = core.assets["sounds/cf001/bgm_clear.mp3"].clone();
+        snd.play();
+        core.stop();
+    }
+
     function gameOver(){
         bgm.stop();
-        var spr = createSprite(160, 140, 230, 32, "images/gameover.png");
+        var spr = createSprite(160, 140, 230, 32, "images/cf001/gameover.png");
         scene.addChild(spr);
-        var snd = core.assets["sounds/cf001/bgm_gameover.mp3"].clone();
+        var snd = core.assets["sounds/cf001/bgm_over.mp3"].clone();
         snd.play();
         core.stop();
     }
