0c06ebf3b1c17512ac0b9168367216e654431a81
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-08-04 14:28:51 +0900

    調整

--- EduFair/ver0.1/main.js
+++ EduFair/ver0.1/main.js
@@ -2,7 +2,7 @@
     教育フェア用教材
 */
 
-const TIME_LIMIT = 10;
+const TIME_LIMIT = 30;
 
 var assets = [
     "images/cf001/b_left.png",    "images/cf001/b_right.png",
@@ -25,7 +25,7 @@ function gameStart(){
     var scene = new Scene();
     core.replaceScene(scene);
 
-    var world = new PhysicsWorld(0, 8);
+    var world = new PhysicsWorld(0, 4);
     scene.addEventListener(Event.ENTER_FRAME, ()=>{world.step(core.fps);});
     scene.backgroundColor = "black";
 
@@ -33,26 +33,27 @@ function gameStart(){
         console.log("Fire!!");
         playSound("sounds/cf001/cannon.mp3");
         bullet.angle = getRandom(0, 90);
-        bullet.addImpulse(0, -30);
+        bullet.addImpulse(4, -8.0);
+        bullet.addImpulse(5, -8.0);
+
     }
 
     var area = new Group();
     scene.addChild(area);
 
-    var sky = new Sprite(320*30, 320*5);
+    var sky = new Sprite(320*30, 320*30);
     sky.image = core.assets["images/cf001/sky.png"];
-    sky.x = 0; 
-    sky.y = sky.height*-4/5;
+    sky.x = -320; sky.y = 320 - sky.height;
     area.addChild(sky);
 
     var back1 = new Sprite(320*30, 120);
     back1.image = core.assets["images/cf001/back1.png"];
-    back1.y = 30;
+    back1.x = -320; back1.y = 30;
     area.addChild(back1);
 
     var back2 = new Sprite(320*30, 200);
     back2.image = core.assets["images/cf001/back2.png"];
-    back2.y = 150;
+    back2.x = -320; back2.y = 150;
     area.addChild(back2);
 
     var title = new Sprite(260, 85);
@@ -65,13 +66,29 @@ function gameStart(){
     bgm.play();
 
     var ground = new PhyBoxSprite(320*30, 64, enchant.box2d.STATIC_SPRITE);
-    ground.x = 0; ground.y = 350-32;
+    ground.x = -320; ground.y = 350-32;
     ground.backgroundColor = "green";
-    ground.tag = "ground";
-    ground.score = -999;
     area.addChild(ground);
 
+    var wL = new PhyBoxSprite(32, 350*30, enchant.box2d.STATIC_SPRITE);
+    wL.x = -320-32; wL.y = 350*-29;
+    wL.backgroundColor = "black";
+    area.addChild(wL);
+
+    var wR = new PhyBoxSprite(32, 350*30, enchant.box2d.STATIC_SPRITE);
+    wR.x = 320*29; wR.y = 350*-29;
+    wR.backgroundColor = "orange";
+    area.addChild(wR);
+
+    var wT = new PhyBoxSprite(350*30, 32, enchant.box2d.STATIC_SPRITE);
+    wT.x = 0; wT.y = 350*-29;
+    wT.backgroundColor = "red";
+    area.addChild(wT);
+
     createTable(100, 280);
+    
+    createBonus(50, -1200, 5, 5, 8, 10, "images/cf001/coin.png", 10);
+    createBonus(2600, -1200, 20, 20, 8, 10, "images/cf001/coin.png", 10);
 
     var offX = 320;
     var offY = 280;
@@ -79,40 +96,60 @@ function gameStart(){
     for(var i=0; i<20; i++){
         var rdm = getRandom(0, 20);
         if(i < 5){
-            createTable(offX+padX*i, offY-rdm, 8, 10, 
-                        "images/cf001/coin.png", "item", 10);
+            createTable(offX+padX*i, offY-rdm, 32, 32, 
+                        "images/cf001/medal.png", 100);
         }else if(i < 8){
             createTable(offX+padX*i, offY-rdm, 32, 32, 
-                        "images/cf001/medal.png", "item", 300);
-        }else if(i < 10){
-            createTable(offX+padX*i, offY-rdm, 32, 32, 
-                        "images/cf001/tebasaki.png", "item", 600);
+                        "images/cf001/tebasaki.png", 300);
         }else if(i < 15){
             createTable(offX+padX*i, offY-rdm, 32, 32, 
-                        "images/cf001/musubi.png", "item", 900);
+                        "images/cf001/musubi.png", 900);
         }else{
             createTable(offX+padX*i, offY-rdm, 36, 60, 
-                        "images/cf001/oshiruko.png", "item", 1200);
+                        "images/cf001/oshiruko.png", 1200);
         }
     }
 
-    var bullet = createBox(100, 225, 18, 23, "images/cf001/bullet.png", 0, true);
+    var bullet = createBox(100, 220, 18, 23, "images/cf001/bullet.png", 0, true);
+    bullet.density = 3; bullet.friction = 0; bullet.restitution = 0;
+    bullet.setAwake(false);
     area.addChild(bullet);
     area.setScrollRange(bullet, 90, 90, 90, 90);
 
-    bullet.addCollision(area);
-    bullet.on(Event.COLLISION, (e)=>{
-        if(e.collision.target.tag == "table"){
-            // Do nothing
-        }
-        if(e.collision.target.tag == "ground"){
-            updateScore(e.collision.target);// Score
-        }
-        if(e.collision.target.tag == "item"){
-            bullet.vx *= 0.2; bullet.vy *= 0.2;// Slow
-            updateScore(e.collision.target);// Score
-            e.collision.target.destroy();
-        }
+    var bLabel = createLabel(bullet.centerX, bullet.y+20, 120, "center");
+    bLabel.font = "10px 'PixelMplus10'";
+    bLabel.text = "*,*";
+    bLabel.baseX = Math.floor(bullet.centerX);
+    bLabel.baseY = Math.floor(bullet.centerY);
+    area.addChild(bLabel);
+
+    bullet.on(Event.ENTER_FRAME, ()=>{
+        // Label
+        bLabel.centerX = bullet.centerX;
+        bLabel.y = bullet.y+30;
+        let x = Math.floor(bullet.centerX) - bLabel.baseX;
+        let y = bLabel.baseY - Math.floor(bullet.centerY);
+        bLabel.text = x + "m," + y + "m";
+        // Contact
+        bullet.contact((spr)=>{
+            if(spr.tag == "table"){
+                updateScore(spr);// Score
+                spr.tag = "";
+            }
+            if(spr.tag == "bonus"){
+                updateScore(spr);// Score
+                spr.tag = "";
+                spr.remove();
+            }
+            if(spr.tag == "item"){
+                bullet.vx = 0; bullet.vy = 0;// Slow
+                updateScore(spr);// Score
+                spr.tag = "";
+                spr.remove();
+            }
+        });
+        // GameOver
+        if(280 < bullet.y) gameOver();
     });
 
     var cannon = new Sprite(60, 60);
@@ -142,7 +179,7 @@ function gameStart(){
     tLabel.text = "TIME:" + time;
     scene.addChild(tLabel);
 
-    function createTable(x, y, iW=8, iH=10, iPath="", tag="", s=10){
+    function createTable(x, y, iW=8, iH=10, iPath="", s=10){
         var podL = new Sprite(29, 53);
         podL.image = core.assets["images/cf001/pod.png"];
         podL.centerX = x - 50; podL.centerY = y;
@@ -157,13 +194,28 @@ function gameStart(){
         area.addChild(deco);
         var table = createBox(x, y-25, 64, 32);
         table.tag = "table";
+        table.score = 50;
         area.addChild(table);
         if(iPath == "") return;
         // Items
         var item = createBox(x, table.y-iH, iW, iH, iPath);
-        item.tag = tag;
+        item.tag = "item";
         item.score = s;
         area.addChild(item);
+    }
+
+    function createBonus(bX, bY, rows, cols, iW=8, iH=10, iPath="", s=10){
+        let pad = 12;
+        for(let r=0; r<rows; r++){
+            for(let c=0; c<cols; c++){
+                let x = bX + c * pad; let y = bY + r * pad;
+                var item = createBox(x, y, iW, iH, iPath, 0, true);
+                item.tag = "bonus"; item.score = s;
+                item.density = 0; item.friction = 0; item.restitution = 0;
+                item.setAwake(false);
+                area.addChild(item);
+            }
+        }
     }
 
     function createBox(x, y, w, h, path="", f=0, type=false){
@@ -202,7 +254,7 @@ function gameStart(){
         var pLabel = createLabel(bullet.centerX, bullet.y-10);
         pLabel.text = target.score; area.addChild(pLabel);
         if(target.score<0) pLabel.color = "red";
-        pLabel.tl.moveBy(0, -30, 16);
+        pLabel.tl.moveBy(0, -30, 32);
         pLabel.tl.then(()=>{pLabel.remove();});
         sLabel.text = "SCORE:" + score;
         // Sound
@@ -260,7 +312,7 @@ var core;
 enchant();
 window.onload = function(){
     core = new Core(320, 350);
-    core.fps = 16;
+    core.fps = 24;
     core.preload(assets);
     core.onload = function(){
         gameStart();
