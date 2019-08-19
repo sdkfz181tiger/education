ae22877fb0c04f365e050c7515f83c2f856b7cb1
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-08-20 08:14:58 +0900

    6人のデザイナー用にファイルをわける

---
+++ Three-js_Meikoudai/04_SoundGame/ver1.2/js/main_designer_2.js
@@ -0,0 +1,37 @@
+
+//==========
+// モデリングデータの使い方
+// MagicaVoxel参考動画
+//     https://www.youtube.com/watch?v=MQPENfEOJJg
+//
+// 1, 3Dモデリングデータは、./models/obj/フォルダに格納しよう
+// 2, モデルデータの読み込みは、"data.js"に記述しよう(models)
+// 3, 背景に配置する場合は"setSceneryRoot()"関数に記述しよう
+// 4, 譜面に配置する場合は"setSceneryNote()"関数に記述しよう
+
+//==========
+// サウンドデータの使い方
+//
+// 1, サウンドデータは、./sounds/フォルダに格納しよう
+// 2, モデルデータの読み込みは、"data.js"に記述しよう(sounds)
+
+//==========
+// 譜面データの編集の仕方
+//
+// 1, 譜面データは、"data.js"の(noteData)変数です
+
+//==========
+// 背景に配置
+function setSceneryRoot2(){
+	let group = rootGroup;
+
+	// "rootGroup(背景)"に配置する
+}
+
+//==========
+// 譜面に配置
+function setSceneryNote2(){
+	let group = noteGroup;
+
+	// "noteGroup(譜面)"に配置する
+}
