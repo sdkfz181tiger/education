01cb3e05ebdfef60ca511a50fe1a6e364a616e9a
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-08-21 01:13:44 +0900

    Finish

---
+++ Meikodai/Final/ver1.2_j2/js/main_designer_5.js
@@ -0,0 +1,21 @@
+
+//==========
+// トゥイーンアニメーションライブラリ
+// TimelineMax
+// 	https://greensock.com/docs/TimelineMax
+
+//==========
+// 背景に配置
+function setSceneryRoot5(){
+	let group = rootGroup;
+
+	// "rootGroup(背景)"に配置する
+}
+
+//==========
+// 譜面に配置
+function setSceneryNote5(){
+	let group = noteGroup;
+
+	// "noteGroup(譜面)"に配置する
+}
