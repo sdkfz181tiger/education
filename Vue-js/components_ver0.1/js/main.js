af4688927a903a61ca9d859f92ba3fc344bdaf4b
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-09-01 12:24:41 +0900

    component

--- Vue-js/components_ver0.1/js/main.js
+++ Vue-js/components_ver0.1/js/main.js
@@ -1,24 +1,11 @@
 console.log("main.js!!");
 
-// Addresses
-let addresses = [
-	{name: "Yamada",    email: "yam@gmail.com", tel:"0120-111-111"},
-	{name: "Joshin",    email: "jos@gmail.com", tel:"0120-222-222"},
-	{name: "Yodobashi", email: "yod@gmail.com", tel:"0120-333-333"},
-	{name: "Bigcamera", email: "big@gmail.com", tel:"0120-444-444"}
-];
-
-// Data
-let data = {
-	items: addresses// Addresses 
-}
+// Component
+Vue.component("hello", {
+	template: "<p>This is hello component!!</p>"
+});
 
 // Vue
 let app = new Vue({
-	el: "#app",
-	data: data
+	el: "#app"
 });
-
-function doAction(){
-	
-}
