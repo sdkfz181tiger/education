6e5e360743a2750e92882d1b9105e312944a3eef
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2019-09-01 21:56:26 +0900

    BMI計算機

--- Vue-js/bmi_ver0.1/js/main.js
+++ Vue-js/bmi_ver0.1/js/main.js
@@ -1,10 +1,21 @@
 console.log("main.js!!");
 
+// Class
+let classObj = {
+	default: true,
+	normal:  false,
+	warning: false,
+	danger:  false
+}
+
 // BMI
 let bmiData = {
-	weight: 0,
-	height: 0,
-	result: 0
+	nWeight: 60,
+	nHeight: 160,
+	nResult: 0,
+	errFlg:  false,
+	errMsg:  "***",
+	classes: classObj
 }
 
 // Vue
@@ -12,25 +23,79 @@ let app = new Vue({
 	el: "#app",
 	data: bmiData,
 	components: {
-		"cmpdisp":{
-			template: '<p>BMI:{{bmiData.result}}</p>'
+		"c_disp":{
+			template: '<p v-bind:class="bmiData.classes">BMI:{{bmiData.nResult}}</p>'
 		},
-		"cmpinput":{
-			template: '<input type="Number" v-on:input="doInput"/>',
+		"c_weight":{
+			template: '<input v-on:input="doCheck" v-model="bmiData.nWeight"/>',
 			methods: {
-				doInput: function(e){
-					console.log("doInput!!");
+				doCheck: function(e){
+					checkData(e.target.value);
 				}
 			}
 		},
-		"cmpctl":{
+		"c_height":{
+			template: '<input v-on:input="doCheck" v-model="bmiData.nHeight"/>',
+			methods: {
+				doCheck: function(e){
+					checkData(e.target.value);
+				}
+			}
+		},
+		"c_ctl":{
 			template: '<button v-on:click="doCalc">Calc</button>',
 			methods: {
 				doCalc: function(e){
-					console.log("doCalc!!");
-					bmiData.result++;
+					calcBmi();
 				}
 			}
 		}
 	}
 });
+
+function checkData(value){
+
+	if(value == ""){
+		bmiData.errFlg = true;
+		bmiData.errMsg = "データを入力してください";
+		return false;
+	}
+
+	if(!isFinite(value)){
+		bmiData.errFlg = true;
+		bmiData.errMsg = "数値データを入力してください";
+		return false;
+	}
+
+	if(value <= 0){
+		bmiData.errFlg = true;
+		bmiData.errMsg = "正しいデータを入力してください";
+		return false;
+	}
+
+	bmiData.errFlg = false;
+	bmiData.errMsg = "***";
+	return true;
+}
+
+function calcBmi(){
+	console.log("calcBmi!!");
+
+	// Calc
+	bmiData.nResult = bmiData.nWeight / Math.pow(bmiData.nHeight*0.01, 2);
+	bmiData.nResult = Math.floor(bmiData.nResult * 100) * 0.01;
+	
+	classObj.default = false;
+	classObj.normal  = false;
+	classObj.warning = false;
+	classObj.danger  = false;
+	if(bmiData.nResult < 18){
+		classObj.danger  = true;
+	}else if(bmiData.nResult < 25){
+		classObj.normal  = true;
+	}else if(bmiData.nResult < 35){
+		classObj.warning = true;
+	}else{
+		classObj.danger  = true;
+	}
+}
