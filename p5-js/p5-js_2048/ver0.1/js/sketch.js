d5fb84210508478f20c518c50bb53b18e44123fb
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-03-20 01:57:29 +0900

    スライド

--- p5-js/p5-js_2048/ver0.1/js/sketch.js
+++ p5-js/p5-js_2048/ver0.1/js/sketch.js
@@ -4,12 +4,10 @@
 
 console.log("Hello p5.js!!");
 
-const DEBUG   = false;
+const DISP_W       = 480;
+const DISP_H       = 320;
 
-const DISP_W  = 480;
-const DISP_H  = 320;
-
-const FONT_SIZE = 32;
+const FONT_SIZE    = 32;
 
 const TILE_TOTAL   = 16;
 const TILE_SIZE    = 62;
@@ -18,50 +16,143 @@ const TILE_CORNER  = 5;
 const TILE_ROWS    = 4;
 const TILE_COLS    = 4;
 
+let my2048;
 let tiles;
-
-function preload(){
-	console.log("preload");
-	tiles = [];
-}
+let sX, sY;
 
 function setup(){
 	createCanvas(DISP_W, DISP_H);
 	frameRate(16);
 	colorMode(HSB);
 
-	let sX = DISP_W / 2 - TILE_PADDING * TILE_COLS / 2;
-	let sY = DISP_H / 2 - TILE_PADDING * TILE_ROWS / 2;
-	let c = color(33, 33, 99);
-	for(let i=0; i<TILE_TOTAL; i++){
-		let x = sX + floor(i % TILE_COLS) * TILE_PADDING;
-		let y = sY + floor(i / TILE_COLS) * TILE_PADDING;
-		let tile = new Tile(99, x, y, c);
-		tiles.push(tile);
+	// 2048
+	my2048 = new Smz2048();
+	my2048.randomPut();
+	my2048.randomPut();
+	my2048.randomPut();
+	my2048.randomPut();
+	my2048.consoleBoard();
+
+	// Tiles
+	sX = DISP_W / 2 - TILE_PADDING * TILE_COLS / 2;
+	sY = DISP_H / 2 - TILE_PADDING * TILE_ROWS / 2;
+	let tC = color(33, 66, 99, 100);
+	let board = my2048.getBoard();
+	tiles = [];
+	for(let r=0; r<TILE_ROWS; r++){
+		let line = [];
+		for(let c=0; c<TILE_COLS; c++){
+			let n = board[r][c];
+			let x = sX + TILE_PADDING * c;
+			let y = sY + TILE_PADDING * r;
+			line.push(new Tile(n, x, y, tC));
+		}
+		tiles.push(line);
 	}
 }
 
 function draw(){
 	background(33, 33, 33);
-	for(let tile of tiles) tile.draw();
+	for(let r=0; r<TILE_ROWS; r++){
+		for(let c=0; c<TILE_COLS; c++){
+			tiles[r][c].draw();
+		}
+	}
+}
+
+function keyPressed(){
+	if(key == "ArrowLeft") actionLeft();
+	if(key == "ArrowRight") actionRight();
+	if(key == "ArrowUp") actionUp();
+	if(key == "ArrowDown") actionDown();
+}
+
+function actionLeft(){
+	if(!my2048.slideLeft()) return;
+	my2048.consoleBoard();
+	updateBoard();
+}
+
+function actionRight(){
+	if(!my2048.slideRight()) return;
+	my2048.consoleBoard();
+	updateBoard();
+}
+
+function actionUp(){
+	if(!my2048.slideUp()) return;
+	my2048.consoleBoard();
+	updateBoard();
+}
+
+function actionDown(){
+	if(!my2048.slideDown()) return;
+	my2048.consoleBoard();
+	updateBoard();
+}
+
+function updateBoard(){
+	let board = my2048.getBoard();
+	for(let r=0; r<TILE_ROWS; r++){
+		for(let c=0; c<TILE_COLS; c++){
+			let move = my2048.getMove(r, c);
+			if(move != null){
+				console.log("to move:", move);
+				tiles[r][c].moveTo(move.gR, move.gC);
+			}
+		}
+	}
+	// TODO: interval
+	// my2048.randomPut();
 }
 
 class Tile{
 
-	constructor(num, x, y, c){
-		this._num = num;
+	constructor(n, x, y, c){
+		this._n = n;
+		this._x = x;
+		this._y = y;
+		this._c = c;
+		this._dX = x;
+		this._dY = y;
+	}
+
+	setNum(n){
+		this._n = n;
+	}
+
+	setPosition(x, y, c){
 		this._x = x;
 		this._y = y;
 		this._c = c;
 	}
 
+	moveTo(gR, gC){
+		this._dX = this._x + gC * TILE_PADDING;
+		this._dY = this._y + gR * TILE_PADDING;
+	}
+
 	draw(){
+		// Move
+		if(this.calcDistance() < 4){
+			this._x = this._dX;
+			this._y = this._dY;
+		}else{
+			this._x += (this._dX - this._x) / 3;
+			this._y += (this._dY - this._y) / 3;
+		}
+		if(this._n == 0) return;
 		// Background
 		noStroke(); fill(this._c);
 		square(this._x, this._y, TILE_SIZE, TILE_CORNER);
 		// Font
-		colorMode(RGB); fill(33, 33, 33);
-		textSize(FONT_SIZE); textAlign(CENTER);
-		text(this._num, this._x+TILE_SIZE/2, this._y+TILE_SIZE-FONT_SIZE*0.6);
+		fill(33, 33, 33); textSize(FONT_SIZE); textAlign(CENTER);
+		text(this._n, this._x+TILE_SIZE/2, this._y+TILE_SIZE-FONT_SIZE*0.6);
+	}
+
+	calcDistance(){
+		let x = this._dX - this._x;
+		let y = this._dY - this._y;
+		return x*x+y*y;
 	}
 }
