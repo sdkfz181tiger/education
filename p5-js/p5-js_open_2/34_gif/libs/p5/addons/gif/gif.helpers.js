eaa5799217b5fe6bf7df794e6c9a139eed0f3b10
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-07-07 07:53:35 +0900

    使いやすく

--- p5-js/p5-js_open_2/34_gif/libs/p5/addons/gif/gif.helpers.js
+++ p5-js/p5-js_open_2/34_gif/libs/p5/addons/gif/gif.helpers.js
@@ -1,5 +1,5 @@
 
-let canvasToRecord = null;
+let canvasToRecord = null; // 録画対象
 let isRecording    = false;// 録画開始フラグ
 let firstFrame     = 16;   // 録画開始フレーム
 let lastFrame      = 48;   // 録画終了フレーム
@@ -19,23 +19,23 @@ function initGIF(canvas, flg, first, last, rate){
 }
 
 function recordGIF() {
-	if (firstFrame > lastFrame) {
-		if (frameCount === 1) {
-		console.log("lastFrame must be a larger value than firstFrame");
-	}
-	} else if (firstFrame < 1) {
-		if (frameCount === 1) {
-		console.log("firstFrame must be larger than or equal to a value of 1");
-	}
-	} else {
-		if (isRecording) {
+	if(firstFrame > lastFrame){
+		if(frameCount === 1){
+			console.log("lastFrame must be a larger value than firstFrame");
+		}
+	}else if (firstFrame < 1){
+		if(frameCount === 1){
+			console.log("firstFrame must be larger than or equal to a value of 1");
+		}
+	}else{
+		if(isRecording){
 			console.log("recording");
-			if (frameCount == firstFrame) {
+			if(frameCount == firstFrame){
 				capturer.start();
 			}
-			if (frameCount < lastFrame) {
+			if(frameCount < lastFrame){
 				capturer.capture(canvasToRecord);
-			} else if (frameCount === lastFrame) {
+			}else if(frameCount === lastFrame){
 				capturer.stop();
 				capturer.save();
 				isRecording = false;
@@ -44,15 +44,15 @@ function recordGIF() {
 	}
 }
 
-function exampleAnimation() {
-	translate(width/2, height/2)
-	fill(205, 200, 100)
+function exampleAnimation(){
+	translate(width/2, height/2);
+	fill(205, 200, 100);
 	noStroke();
-	ellipse(0, 0, width/3)
-	stroke(0)
-	strokeWeight(3)
-	arc(0, 0, width/5, width/5, 0, PI)
-	fill(0)
-	ellipse(-30, -30, 20, constrain(sin(frameCount*0.05)+1, 0, 1)*20)
-	ellipse(30, -30, 20)
+	ellipse(0, 0, width/3);
+	stroke(0);
+	strokeWeight(3);
+	arc(0, 0, width/5, width/5, 0, PI);
+	fill(0);
+	ellipse(-30, -30, 20, constrain(sin(frameCount*0.05)+1, 0, 1)*20);
+	ellipse(30, -30, 20);
 }
