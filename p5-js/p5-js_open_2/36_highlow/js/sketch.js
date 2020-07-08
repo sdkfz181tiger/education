5c330f616733f776266dab76633129c78c33ce8c
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-07-08 21:48:28 +0900

    コード整理

---
+++ p5-js/p5-js_open_2/36_highlow/js/sketch.js
@@ -0,0 +1,32 @@
+"use strict";
+//==========
+// p5.js
+// Original code link:
+// ***
+
+let cX, cY;
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	frameRate(8);
+	angleMode(DEGREES);
+	rectMode(CENTER);
+	background(33);
+
+	cX = width / 2;
+	cY = height / 2;
+}
+
+function draw(){
+	background(33);
+
+
+
+}
+
+class Dot{
+
+	constructor(){
+		this._x = 0;
+	}
+}
