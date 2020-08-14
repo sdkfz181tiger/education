24eb8390a15065b5a8019c8c2c31a9d9778436f5
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-08-14 12:01:11 +0900

    とりま

--- p5-js/p5-js_open_original/03_rotate_5/js/sketch.js
+++ p5-js/p5-js_open_original/03_rotate_5/js/sketch.js
@@ -3,36 +3,60 @@
 // p5.js
 
 let colors = ["#386641", "#6A994E", "#A7C957", "#F2E8CF", "#BC4749"];
-let cX, cY, radius;
+let dLines = [];
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 	angleMode(DEGREES);
 	rectMode(CENTER);
-	noLoop();
+	frameRate(32);
 	noFill();
 	stroke(255);
 	strokeWeight(0.5);
 
-	cX = width / 2;
-	cY = height / 2;
-	radius = 10;
+	let x = width / 2;
+	let y = height / 2;
+
+	for(let i=0; i<360; i+=10){
+		let dLine = new DotLine(x, y, i, width/10, 5);
+		dLines.push(dLine);
+	}
 }
 
 function draw(){
 	background(33);
 
-	let size = 5;
-	for(let i=0; i<360; i+=30){
+	for(let dLine of dLines) dLine.draw();
+}
+
+class DotLine{
+
+	constructor(x, y, r, s, len){
+		this._x = x;
+		this._y = y;
+		this._r = r;
+		this._s = s;
+		this._len = len;
+		this._rot = 0;
+		this._arr = [];
+		for(let i=0; i<len; i++){
+			this._arr.push(s);
+		}
+	}
+
+	draw(){
+
+		this._rot += 4;
+		if(360 < this._rot) this._rot -= 360;
 		push();
-		translate(cX, cY);
-		console.log(i);
-		rotate(i);
-		for(let j=0; j<10; j++){
-			let x = size * j;
-			let y = 0;
-			let s = random(2, 5);
-			square(x, y, s);
+		translate(this._x, this._y);
+		rotate(this._r);
+		let pX = this._s;
+		let pR = 360 / this._len;
+		for(let i=0; i<this._len; i++){
+			stroke(colors[i%colors.length]);
+			let size = this._arr[i] * sin(this._rot+pR*i); 
+			square(pX*i, 0, size);
 		}
 		pop();
 	}
---
+++ p5-js/p5-js_open_original/03_rotate_6/js/sketch.js
@@ -0,0 +1,66 @@
+"use strict";
+//==========
+// p5.js
+
+let colors = ["#386641", "#6A994E", "#A7C957", "#F2E8CF", "#BC4749"];
+let dLines = [];
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	angleMode(DEGREES);
+	rectMode(CENTER);
+	frameRate(16);
+	noFill();
+	stroke(255);
+	strokeWeight(1);
+
+	let x = width / 2;
+	let y = height / 2;
+
+	for(let i=0; i<360; i+=30){
+		let dLine = new DotLine(x, y, i, width/10, 5);
+		dLines.push(dLine);
+	}
+}
+
+function draw(){
+	background(33);
+
+	for(let dLine of dLines) dLine.draw();
+}
+
+class DotLine{
+
+	constructor(x, y, r, s, len){
+		this._x = x;
+		this._y = y;
+		this._r = r;
+		this._s = s;
+		this._len = len;
+		this._rot = 0;
+		this._arr = [];
+		for(let i=0; i<len; i++){
+			this._arr.push(s);
+		}
+	}
+
+	draw(){
+		this._rot += 4;
+		if(360 < this._rot) this._rot -= 360;
+		push();
+		translate(this._x, this._y);
+		rotate(this._r);
+		let pX = this._s;
+		let pR = 360 / this._len;
+		for(let i=0; i<this._len; i++){
+			stroke(colors[i%colors.length]);
+			let size = this._arr[i] * cos(this._rot+pR*i);
+			if(size < 0){
+				circle(pX*i, 0, size);
+			}else{
+				square(pX*i, 0, size);
+			}
+		}
+		pop();
+	}
+}
