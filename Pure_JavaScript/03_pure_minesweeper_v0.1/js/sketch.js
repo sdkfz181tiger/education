f305f298e7ee34839100493a6729f2d530108ef3
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-08-26 21:07:31 +0900

    ソースコード整理

--- Pure_JavaScript/03_pure_minesweeper_v0.1/js/sketch.js
+++ Pure_JavaScript/03_pure_minesweeper_v0.1/js/sketch.js
@@ -4,9 +4,10 @@
 
 const WIDTH  = 320;
 const HEIGHT = 240;
-const ROWS   = 15;
+const ROWS   = 9;
 const COLS   = 10;
-const SIZE   = 16;
+const PAD    = 24;
+const SIZE   = 22;
 
 // Context
 const canvas  = document.getElementById("canvas");
@@ -15,26 +16,39 @@ canvas.height = HEIGHT;
 
 const ctx = canvas.getContext("2d");
 
-// Update
-update();
-function update(){
+const oX = Math.floor(WIDTH / 2 - COLS * PAD / 2);
+const oY = Math.floor(HEIGHT / 2 - ROWS * PAD / 2);
 
-	let sX = Math.floor(WIDTH / 2 - COLS * SIZE / 2);
-	let sY = Math.floor(HEIGHT / 2 - ROWS * SIZE / 2);
+let msMng = new MineSweeper(ROWS, COLS, 5);
+show();
 
+function show(){
 	// Background
 	ctx.fillStyle = "#666666";
 	ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
 	ctx.fillStyle = "#999999";
-	ctx.fillRect(sX, sY, COLS*SIZE, ROWS*SIZE);
+	ctx.fillRect(oX, oY, COLS*PAD, ROWS*PAD);
 
-	setTimeout(update, 1000);
+	ctx.fillStyle = "#333399";
+	for(let r=0; r<ROWS; r++){
+		for(let c=0; c<COLS; c++){
+			let x = oX + c * PAD;
+			let y = oY + r * PAD;
+			ctx.fillRect(x, y, SIZE, SIZE);
+		}
+	}
+	msMng.consoleAll();
 }
 
 // Keyboard
-document.addEventListener("keydown", (e)=>{
-	
+document.addEventListener("click", (e)=>{
+	let r = Math.floor((e.y - oY) / PAD);
+	let c = Math.floor((e.x - oX) / PAD);
+	if(msMng.search(r, c)){
+		console.log("GAME OVER");
+	}
+	show();
+
+	console.log(r, c, msMng.getCell(r, c));
 });
-
-
--- p5-js/p5-js_game/minesweeper_1.0/ver0.1/js/minesweeper.js
+++ p5-js/p5-js_game/minesweeper_1.0/ver0.1/js/minesweeper.js
@@ -1,14 +1,4 @@
-/*!
- * smz2048.js v1.0.0
- *
- * Copyright (c) 2020 ShimejiOzaki
- *
- * Released under the MIT license.
- * see http://ozateck.sakura.ne.jp
- *
- * The inherits function is:
- * ISC license | https://github.com/isaacs/inherits/blob/master/LICENSE
- */
+
 class MineSweeper{
 
 	constructor(rows, cols, mines){
