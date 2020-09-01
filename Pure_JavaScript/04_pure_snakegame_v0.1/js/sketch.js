677ae10fcc6684b999d5408e902fbccd3277a265
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-09-01 22:57:01 +0900

    Snake

--- Pure_JavaScript/04_pure_snakegame_v0.1/js/sketch.js
+++ Pure_JavaScript/04_pure_snakegame_v0.1/js/sketch.js
@@ -4,46 +4,79 @@
 
 const WIDTH  = 320;
 const HEIGHT = 320;
-const ROWS   = 9;
-const COLS   = 9;
-const PAD    = 32;
-const SIZE   = 30;
+const WALL   = 8;
+const SIZE   = 24;
 
-let canvas, ctx, oX, oY;
-let sgMng;
+let canvas, ctx, sMng;
 
-// On loaded
+// Window
 window.addEventListener("load", (e)=>{
-	console.log("Hello");
-	// Context
+	
+	// Canvas and Context
 	canvas  = document.getElementById("canvas");
 	canvas.width  = WIDTH;
 	canvas.height = HEIGHT;
 
 	ctx = canvas.getContext("2d");
-	ctx.font = SIZE + "px Arial";
+	ctx.font = "24px Arial";
 	ctx.textAlign = "center";
 
-	oX = Math.floor(WIDTH / 2 - COLS * PAD / 2);
-	oY = Math.floor(HEIGHT / 2 - ROWS * PAD / 2);
-
-	sgMng = new SnakeGame(ROWS, COLS);
-	show();
+	// Manager
+	sMng = new SnakeGame(WIDTH, HEIGHT, SIZE);
+	update();
 });
 
-function show(){
+function update(){
 	// Background
-	ctx.fillStyle = "#333333";
+	ctx.fillStyle = "#993333";
 	ctx.fillRect(0, 0, WIDTH, HEIGHT);
-
-	ctx.fillStyle = "#444444";
-	ctx.fillRect(oX, oY, COLS*PAD-1, ROWS*PAD-1);
+	ctx.fillStyle = "#333333";
+	ctx.fillRect(WALL, WALL, WIDTH-WALL*2, HEIGHT-WALL*2);
+	// Update
+	sMng.update();
+	// Body(Tail)
+	let body = sMng.getBody();
+	for(let b=1; b<body.length; b++){
+		ctx.fillStyle = "#6699ff";
+		ctx.fillRect(body[b].x, body[b].y, SIZE, SIZE);
+	}
+	// Body(Head)
+	ctx.fillStyle = "#ff6633";
+	ctx.fillRect(body[0].x, body[0].y, SIZE, SIZE);
+	// Foods
+	let foods = sMng.getFoods();
+	for(let food of foods){
+		ctx.fillStyle = "#33dd33";
+		ctx.fillRect(food.x, food.y, SIZE, SIZE);
+	}
+	// GameOver
+	if(sMng.isGameOver()){
+		console.log("GAME OVER!!");
+		ctx.fillStyle = "#ffffff";
+		ctx.fillText("GAME OVER!!", WIDTH/2, HEIGHT/2+SIZE/2);
+	}else{
+		setTimeout(update, 200);// Timeout
+	}
 }
 
 // Keyboard
-document.addEventListener("click", (e)=>{
-	let r = Math.floor((e.y - oY) / PAD);
-	let c = Math.floor((e.x - oX) / PAD);
-	
-	show();
+document.addEventListener("keydown", (e)=>{
+	// Key
+	let key = e.keyCode;
+	// Left
+	if(key == 37){
+		sMng.goLeft();
+	}
+	// Right
+	if(key == 39){
+		sMng.goRight();
+	}
+	// Down
+	if(key == 40){
+		sMng.goDown();
+	}
+	// Up
+	if(key == 38){
+		sMng.goUp();
+	}
 });
--- Pure_JavaScript/04_pure_snakegame_v0.1/js/utility.js
+++ Pure_JavaScript/04_pure_snakegame_v0.1/js/utility.js
@@ -4,72 +4,125 @@
 
 class SnakeGame{
 
-	constructor(rows, cols){
-		this._rows  = rows;
-		this._cols  = cols;
-		this._table = [];
-		this._snake = [];
+	constructor(width, height, g){
+		this._width  = width;
+		this._height = height;
+		this._g      = g;
+		this._rows   = Math.floor(this._height / g);
+		this._cols   = Math.floor(this._height / g);
+		this._body   = [];
+		this._foods  = [];
 		this.init();
 	}
 
 	init(){
-		// Table
-		for(let r=0; r<this._rows; r++){
-			let line = [];
-			for(let c=0; c<this._cols; c++) line.push(0);
-			this._table.push(line);
-		}
-		// Snake
-		this._snake.push(new Dot(0, 0, 16));
-
-		this.consoleTable(this._table);
+		let x = Math.floor(this._cols/2) * this._g;
+		let y = Math.floor(this._rows/2) * this._g;
+		this._body.push(new Dot(x, y));
+		this._vX = 0;
+		this._vY = 1;
+		this.addFood();
+		this.addFood();
+		this.addFood();
+		this.addFood();
+		this.addFood();
+		this.addFood();
 	}
 
-	consoleTable(table){
-		let line = "";
-		for(let c=0; c<this._cols*2; c++) line += "-";
-		line += "-\n";
-		for(let r=0; r<this._rows; r++){
-			line += "|";
-			for(let c=0; c<this._cols; c++){
-				let n = table[r][c];
-				line += (n==0)?"-":n;
-				if(c < this._cols-1) line += " ";
-			}
-			line += "|\n";
+	addBody(){
+		let t = this._body.length-1;
+		let x = this._body[t].x;
+		let y = this._body[t].y;
+		this._body.push(new Dot(x, y));
+	}
+
+	addFood(){
+		let x = Math.floor(Math.random()*(this._cols-2)+1)*this._g;
+		let y = Math.floor(Math.random()*(this._rows-2)+1)*this._g;
+		this._foods.push(new Dot(x, y));
+	}
+
+	isGameOver(){
+		// Game Area
+		if(this._body[0].x < this._g*0.5) return true;
+		if(this._body[0].y < this._g*0.5) return true;
+		if(this._width-this._g*1.5  < this._body[0].x) return true;
+		if(this._height-this._g*1.5 < this._body[0].y) return true;
+		// Head x Tail
+		for(let f=this._body.length-1; 1<f; f--){
+			let dX = this._body[0].x - this._body[f].x;
+			let dY = this._body[0].y - this._body[f].y;
+			let dist = Math.sqrt(dX*dX + dY*dY);
+			if(dist < this._g) return true;
 		}
-		for(let c=0; c<this._cols*2; c++) line += "-";
-		line += "-\n";
-		console.log(line);
+		return false;
+	}
+
+	update(){
+		// Tail
+		for(let t=this._body.length-1; 0<t; t--){
+			this._body[t].x = this._body[t-1].x;
+			this._body[t].y = this._body[t-1].y;
+		}
+		// Head
+		this._body[0].x += Math.floor(this._g * this._vX);
+		this._body[0].y += Math.floor(this._g * this._vY);
+		// Head x Foods
+		for(let f=this._foods.length-1; 0<=f; f--){
+			let dX = this._body[0].x - this._foods[f].x;
+			let dY = this._body[0].y - this._foods[f].y;
+			let dist = Math.sqrt(dX*dX + dY*dY);
+			if(dist < this._g){
+				this.addBody();          // Add body
+				this.addFood();          // Add food
+				this._foods.splice(f, 1);// Remove food
+			}
+		}
+	}
+
+	getBody(){
+		return this._body;
+	}
+
+	getFoods(){
+		return this._foods;
+	}
+
+	goLeft(){
+		if(this._vX == 1) return;
+		this._vX = -1;
+		this._vY = 0;
+	}
+
+	goRight(){
+		if(this._vX == -1) return;
+		this._vX = 1;
+		this._vY = 0;
+	}
+
+	goUp(){
+		if(this._vY == 1) return;
+		this._vX = 0;
+		this._vY = -1;
+	}
+
+	goDown(){
+		if(this._vY == -1) return;
+		this._vX = 0;
+		this._vY = 1;
 	}
 }
 
 class Dot{
 
-	constructor(r, c, size){
-		this._r = r;
-		this._c = c;
-		this._size = size;
+	constructor(x, y){
+		this._x = x;
+		this._y = y;
 	}
 
-	set r(num){
-		this._r = num;
-	}
-
-	get r(){
-		return this._r;
-	}
-
-	set c(num){
-		this._c = num;
-	}
-
-	get c(){
-		return this._c;
-	}
-
-	get size(){
-		return this._size;
-	}
+	set x(num){this._x = num;}
+	set y(num){this._y = num;}
+	get x(){return this._x;}
+	get y(){return this._y;}
 }
 
