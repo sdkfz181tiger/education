c9dc6c686c36523816b502fe598883084e0894bb
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-09-07 10:47:16 +0900

    もう少し

--- Pure_JavaScript/05_pure_outrun_v0.1/js/sketch.js
+++ Pure_JavaScript/05_pure_outrun_v0.1/js/sketch.js
@@ -9,7 +9,7 @@ const R_DEPTH = 10; // Road depth
 const R_WIDTH = 480;// Road width
 
 let canvas, ctx;
-let posX, posZ, lines;
+let posX, posY, posZ, lines;
 
 // Window
 window.addEventListener("load", (e)=>{
@@ -25,15 +25,23 @@ window.addEventListener("load", (e)=>{
 	ctx.lineWidth   = 2;
 	// Position
 	posX = 0;
+	posY = 200;
 	posZ = 0;
 	// Lines
 	lines = [];
 	for(let i=0; i<400; i++){
 		let line = new Line();
+
 		if(100 < i && i < 150) line.curve = 0.8;
 		if(150 < i && i < 200) line.curve = -0.8;
 		if(250 < i && i < 300) line.curve = 0.8;
 		if(300 < i && i < 350) line.curve = -0.8;
+
+		if(100 < i && i < 130) line.bank = 0.4;
+		if(130 < i && i < 260) line.bank = -0.8;
+		if(190 < i && i < 210) line.bank = 0.4;
+		if(240 < i && i < 270) line.bank = -0.8;
+
 		line.project(0, 200, R_DEPTH*i);
 		lines.push(line);
 	}
@@ -47,20 +55,43 @@ window.addEventListener("load", (e)=>{
 		// Position
 		posZ += 5;
 
-		let x = 0;
-		let dX = 0;
-
 		// Lines
 		const start = Math.floor(posZ/R_DEPTH)+1;
-		for(let i=start; i<start+100; i++){
+
+		let x  = 0;
+		let dX = 0;
+
+		let y  = 0;
+		let dY = 0;
+
+		for(let i=start; i<start+50; i++){
 			let iA = i % lines.length;
 			let iB = (0<iA)?iA-1:lines.length-1;
 			let lA = lines[iA];
 			let lB = lines[iB];
-			lA.project(posX-x, 170, R_DEPTH*i-posZ);
+			lA.project(posX-x, posY-y, R_DEPTH*i-posZ);
 
-			x += dX;
-			dX += lA.curve;
+			// x += dX;
+			// dX += lA.curve;
+
+			// y += dY;
+			// dY += lA.bank;
+
+			// let cGrass = (i%2==0) ? "#33dd33":"#33aa33";
+			// let cSide  = (i%2==0) ? "#333333":"#ffffff";
+			// let cRoad  = (i%2==0) ? "#bbbbbb":"#eeeeee";
+			// drawTrp(lA.X, lA.Y, WIDTH*4, lB.X, lB.Y, WIDTH*4, cGrass);
+			// drawTrp(lA.X, lA.Y, lA.W*1.2, lB.X, lB.Y, lB.W*1.2, cSide);
+			// drawTrp(lA.X, lA.Y, lA.W, lB.X, lB.Y, lB.W, cRoad);
+			// if(iA == 0) drawLine(lA.X, lA.Y, lA.W, "#ff3333");
+		}
+
+		for(let i=start; i<start+50; i++){
+			let iA = i % lines.length;
+			let iB = (0<iA)?iA-1:lines.length-1;
+			let lA = lines[iA];
+			let lB = lines[iB];
+			
 
 			let cGrass = (i%2==0) ? "#33dd33":"#33aa33";
 			let cSide  = (i%2==0) ? "#333333":"#ffffff";
@@ -68,9 +99,25 @@ window.addEventListener("load", (e)=>{
 			drawTrp(lA.X, lA.Y, WIDTH*4, lB.X, lB.Y, WIDTH*4, cGrass);
 			drawTrp(lA.X, lA.Y, lA.W*1.2, lB.X, lB.Y, lB.W*1.2, cSide);
 			drawTrp(lA.X, lA.Y, lA.W, lB.X, lB.Y, lB.W, cRoad);
-
 			if(iA == 0) drawLine(lA.X, lA.Y, lA.W, "#ff3333");
 		}
+
+		// for(let i=start+100; 0<i; i--){
+		// 	let iA = i % lines.length;
+		// 	let iB = (0<iA)?iA-1:lines.length-1;
+		// 	let lA = lines[iA];
+		// 	let lB = lines[iB];
+
+		// 	let cGrass = (i%2==0) ? "#33dd33":"#33aa33";
+		// 	let cSide  = (i%2==0) ? "#333333":"#ffffff";
+		// 	let cRoad  = (i%2==0) ? "#bbbbbb":"#eeeeee";
+		// 	drawTrp(lA.X, lA.Y, WIDTH*4, lB.X, lB.Y, WIDTH*4, cGrass);
+		// 	drawTrp(lA.X, lA.Y, lA.W*1.2, lB.X, lB.Y, lB.W*1.2, cSide);
+		// 	drawTrp(lA.X, lA.Y, lA.W, lB.X, lB.Y, lB.W, cRoad);
+
+		// 	if(iA == 0) drawLine(lA.X, lA.Y, lA.W, "#ff3333");
+		// }
+
 		setTimeout(update, 20);
 	}
 });
@@ -107,6 +154,7 @@ class Line{
 		this._Y = 0;
 		this._W = 0;
 		this._C = 0;
+		this._B = 0;
 	}
 
 	project(x, y, z){
@@ -118,6 +166,9 @@ class Line{
 
 	set curve(n){this._C=n;}
 	get curve(){return this._C;}
+
+	set bank(n){this._B=n;}
+	get bank(){return this._B;}
 
 	get X(){return this._X;}
 	get Y(){return this._Y;}
