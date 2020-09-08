f27a7179dad4cefb99b7ae8d2d43938ab7692df5
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-09-08 19:16:04 +0900

    フォルダ整理

---
+++ JS_p5-js/p5-js_open_original/06_tsubuyaki_mane/js/sketch.js
@@ -0,0 +1,29 @@
+//==========
+// p5.js
+
+w=400;
+c=w/2;
+t=0;
+
+setup=()=>{
+	createCanvas(w,w);
+	//angleMode(DEGREES);
+	frameRate(32);
+	noFill();
+	stroke(255);
+}
+
+draw=()=>{
+	//background(0);
+
+	fill(33, 33, 33, 33)
+	square(0, 0, w, w);
+
+	t += 0.1;
+	for(r=0; r<TAU*9; r+=0.1){
+		let s = sin(t+r*0.3) + 9;
+		let x = cos(r+t)*r*s+c;
+		let y = sin(r+t)*r*s+c;
+		circle(x, y, sin(r) * 9);
+	}
+}
