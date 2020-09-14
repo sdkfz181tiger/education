99e386e36b46ca482c3d4cbaa9eae146881563ff
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-09-14 15:45:00 +0900

    なんとなく...

--- JS_p5-js/p5-js_open_3/04_syakutori/js/sketch.js
+++ JS_p5-js/p5-js_open_3/04_syakutori/js/sketch.js
@@ -1,13 +1,14 @@
 //==========
 // p5.js
 
+const size = 5;
 
-let size, rows, cols, x, y;
-let deg, img;
+let rows, cols;
+let img, deg, scale, offX;
 
 function preload(){
 
-	img = loadImage("images/dali.jpg");
+	img = loadImage("images/pic1.jpg");
 }
 
 function setup(){
@@ -16,37 +17,50 @@ function setup(){
 	noLoop();
 	background(33);
 	stroke(255);
+	strokeWeight(0.5);
 	noFill(255);
 
-	size = 10;
 	rows = height / size;
 	cols = width / size;
-	x = size * floor(cols/2);
-	y = size * floor(rows/2);
+
 	deg = 0;
+	scale = (width<height)?width/img.width:height/img.height;
+	offX = width/2 - img.width*scale/2;
 
-	background(11);
-
-	let scale = (width<height)?width/img.width:height/img.height;
 	let iWidth = img.width * scale;
 	let iHeight = img.height * scale;
-	console.log(scale);
-	image(img, 0, 0, iWidth, iHeight);
+
+	background(11);
+	//image(img, offX, 0, iWidth, iHeight);
 
 	stroke(33, 33, 33, 33);
 	for(let r=0; r<rows; r++){
 		for(let c=0; c<cols; c++){
-			circle(c*size, r*size, size);
+			let tX = c*size;
+			let tY = r*size;
+			let pX = (tX-offX)/scale;
+			let pY = tY/scale;
+			let color = img.get(pX, pY);
+			let avg = (color[0] + color[1] + color[2])/3;
+			if(avg < 50) continue;
+			fill(color);
+			noStroke();
+			circle(tX, tY, size/2);
+			noFill();
+			stroke(color);
+			drawSnake(tX, tY, 10);
 		}
 	}
 }
 
 function draw(){
 	
-	stroke(255);
+}
+
+function drawSnake(x, y, n){
 	push();
 	translate(x, y);
-	for(let i=0; i<100; i++){
+	for(let i=0; i<n; i++){
 		let d = (random()<0.5)?90:-90;
 		stepNext(d);
 	}
