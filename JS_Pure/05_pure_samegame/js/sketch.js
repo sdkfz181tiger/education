9d587183010cd5f8fc284de194bcdc4669b313d8
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2020-09-19 17:13:55 +0900

    動いた

--- JS_Pure/05_pure_samegame/js/sketch.js
+++ JS_Pure/05_pure_samegame/js/sketch.js
@@ -2,11 +2,13 @@
 //==========
 // JavaScript
 
+const palette = ["#233D4D", "#FE7F2D", "#FCCA46", "#A1C181", "#619B8A"];
+
 const WIDTH  = 320;
 const HEIGHT = 320;
 
-const ROWS   = 4;
-const COLS   = 4;
+const ROWS   = 6;
+const COLS   = 6;
 const T_SIZE = 32;
 
 let canvas, ctx, sMng;
@@ -27,7 +29,7 @@ window.addEventListener("load", (e)=>{
 	let sX = WIDTH/2  - COLS*T_SIZE/2;
 	let sY = HEIGHT/2 - ROWS*T_SIZE/2;
 	sMng = new SamegameManager(sX, sY);
-	
+	sMng.checkMtx();// Test
 	update();// Update
 });
 
@@ -39,21 +41,19 @@ function update(){
 	// Text
 	ctx.fillStyle = "#ffffff";
 	ctx.fillText("SameGame!!", WIDTH/2, 32);
-
-	// Test
+	// Matrix
+	let mtx = sMng.getMtx();
 	for(let r=0; r<ROWS; r++){
 		for(let c=0; c<COLS; c++){
-			// Tile
-			let tile = sMng.mtx[r][c];
+			let tile = mtx[r][c];
 			if(tile == null) continue;
-			ctx.fillStyle = "#666666";
+			ctx.fillStyle = palette[Math.floor(tile.type%palette.length)];
 			ctx.fillRect(tile.x, tile.y, T_SIZE-2, T_SIZE-2);
 			ctx.fillStyle = "#ffffff";
 			ctx.fillText(tile.type, tile.x+T_SIZE*0.5, tile.y+T_SIZE*0.7);
 		}
 	}
-
-	//setTimeout(update, 20);
+	setTimeout(update, 500);
 }
 
 function drawLine(x, y, w, c){
@@ -76,14 +76,7 @@ function drawTrp(x1, y1, w1, x2, y2, w2, c){
 }
 
 document.addEventListener("click", (e)=>{
-	for(let r=0; r<ROWS; r++){
-		for(let c=0; c<COLS; c++){
-			let tile = sMng.mtx[r][c];
-			if(tile.isInside(e.x, e.y)){
-				console.log("Capture:", r, c);
-			}
-		}
-	}
+	sMng.touchTiles(e.x, e.y);
 });
 
 //==========
@@ -95,6 +88,7 @@ class SamegameManager{
 		this._sX = sX;
 		this._sY = sY;
 		this.initMatrix();
+		this.compressV();
 	}
 
 	initMatrix(){
@@ -108,29 +102,25 @@ class SamegameManager{
 				this._mtx[r][c] = new Tile(r, c, x, y, type);
 			}
 		}
-		this.checkMtx();
 	}
 
-	// compressV(mtxBef){
-	// 	let mtxAft = createMatrix();
-	// 	for(let c=C_MAX-1; 0<=c; c--){
-	// 		for(let r=R_MAX-1; 0<=r; r--){
-	// 			if(mtxBef[r][c] == null){
-	// 				for(let v=r-1; 0<=v; v--){
-	// 					if(mtxBef[v][c] == null) continue;
-	// 					mtxAft[r][c]   = mtxBef[v][c];
-	// 					mtxAft[r][c].r = r;
-	// 					mtxAft[r][c].c = c;
-	// 					mtxBef[v][c]   = null;
-	// 					break;
-	// 				}
-	// 			}else{
-	// 				mtxAft[r][c] = mtxBef[r][c];
-	// 			}
-	// 		}
-	// 	}
-	// 	return mtxAft;
-	// }
+	compressV(){
+		for(let c=COLS-1; 0<=c; c--){
+			for(let r=ROWS-1; 0<=r; r--){
+				if(this._mtx[r][c] == null){
+					for(let v=r-1; 0<=v; v--){
+						if(this._mtx[v][c] == null) continue;
+						this._mtx[r][c] = this._mtx[v][c];// Swap
+						this._mtx[v][c] = null;
+						let x = this._sX + T_SIZE * c;// Change
+						let y = this._sY + T_SIZE * r;
+						this._mtx[r][c].setParams(r, c, x, y);
+						break;
+					}
+				}
+			}
+		}
+	}
 
 	createMtx(){
 		let mtx = [];
@@ -142,6 +132,10 @@ class SamegameManager{
 		return mtx;
 	}
 
+	getMtx(){
+		return this._mtx;
+	}
+
 	checkMtx(){
 		let bar = "";
 		for(let b=0; b<COLS*2+3; b++) bar += "=";
@@ -150,7 +144,7 @@ class SamegameManager{
 		for(let r=0; r<ROWS; r++){
 			let line = "| ";
 			for(let c=0; c<COLS; c++){
-				line += (this._mtx[r][c]==null) ? "  ":this._mtx[r][c].type+" ";
+				line += (this._mtx[r][c]==null)?"  ":this._mtx[r][c].type+" ";
 			}
 			str += line + "|\n";
 		}
@@ -158,26 +152,71 @@ class SamegameManager{
 		console.log(str);
 	}
 
-	touchTile(tX, tY){
+	touchTiles(tX, tY){
+		// Search
+		this._chains = [];
 		for(let r=0; r<ROWS; r++){
 			for(let c=0; c<COLS; c++){
-				return this._mtx[r][c].isInside(tX, tY);
+				let tile = this._mtx[r][c];
+				if(tile == null) continue;
+				if(tile.isInside(tX, tY)){
+					this.searchTiles(tile);
+				}
 			}
+		}
+		// Remove
+		if(this._chains.length < 2) return;
+		for(let tile of this._chains){
+			this._mtx[tile.r][tile.c] = null;// Remove
+		}
+		this.compressV();// Compress
+		this.checkMtx();// Check
+	}
+
+	searchTiles(tile){
+		//console.log("searchTiles:", tile.r, tile.c);
+		this._chains.push(tile);// Push
+		this.traseTile(tile, 0, 1);
+		this.traseTile(tile, 0,-1);
+		this.traseTile(tile, 1, 0);
+		this.traseTile(tile,-1, 0);
+	}
+
+	isExists(tile){
+		for(let target of this._chains){
+			if(tile.r != target.r) continue;
+			if(tile.c != target.c) continue;
+			return true;
 		}
 		return false;
 	}
 
-	get mtx(){return this._mtx;}
+	traseTile(tile, oR, oC){
+		//console.log("traseTile", oR, oC)
+		if(tile.r+oR < 0) return;
+		if(tile.c+oC < 0) return;
+		if(ROWS-1 < tile.r+oR) return;
+		if(COLS-1 < tile.c+oC) return;
+		let target = this._mtx[tile.r+oR][tile.c+oC];
+		if(target == null) return;
+		if(tile.type != target.type) return;
+		if(this.isExists(target)) return;
+		this.searchTiles(target);
+	}
 }
 
 class Tile{
 
 	constructor(r, c, x, y, type){
+		this.setParams(r, c, x, y);
+		this._type = type;
+	}
+
+	setParams(r, c, x, y){
 		this._r = r;
 		this._c = c;
 		this._x = x;
 		this._y = y;
-		this._type = type;
 	}
 
 	isInside(tX, tY){
