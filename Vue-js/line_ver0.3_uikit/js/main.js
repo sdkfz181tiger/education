cd96d74b5e357afe316d48e6b72955cfe4013f88
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-04-30 23:50:03 +0900

    Refactor

--- Vue-js/line_ver0.3_uikit/js/main.js
+++ Vue-js/line_ver0.3_uikit/js/main.js
@@ -4,7 +4,8 @@ console.log("main.js!!");
 let myData = {
 	txName: "",
 	txData: "",
-	txLogs: []
+	txLogs: [],
+	tnBase64: ""
 }
 
 // Vue
@@ -12,7 +13,20 @@ let app = new Vue({
 	el: "#app",
 	data: myData,
 	methods:{
-
+		createThumbnail(e){
+			// Base64
+			const files = e.target.files || e.dataTransfer.files;
+			this.getBase64(files[0]).then(image=>this.tnBase64 = image).catch(error=>console.log(error));
+		},
+		getBase64(file){
+			// Promise
+			return new Promise((resolve, reject)=>{
+				const reader = new FileReader();
+				reader.onload = () => resolve(reader.result);
+				reader.onerror = error => reject(error);
+				reader.readAsDataURL(file);
+			});
+		}
 	},
 	computed:{
 		txInput:{
@@ -34,10 +48,8 @@ let app = new Vue({
 	},
 	watch:{
 		txData:(value)=>{
-
 			// Clear
 			myData.txLogs.length = 0;// Important
-
 			// Split
 			const regLine = /\n|\r\n|\r/;
 			const regSpace = /^ |^　/;
@@ -45,9 +57,9 @@ let app = new Vue({
 			for(let line of lines){
 				if(line.length == 0 || line == " ") continue;
 				if(line.match(regSpace) != null){
-					myData.txLogs.push({name: "", text: line, time: "00:04:00", youFlg: true});
+					myData.txLogs.push({name: "", text: line, youFlg: true});
 				}else{
-					myData.txLogs.push({name: myData.txName, text: line, time: "00:04:00", youFlg: false});
+					myData.txLogs.push({name: myData.txName, text: line, youFlg: false});
 				}
 			}
 		}
