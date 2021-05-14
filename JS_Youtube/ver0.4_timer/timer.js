f6a6977cddf1db3320d4aa178f1cc03729c80242
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-05-14 17:40:00 +0900

    vue.js用にテンプレート

--- JS_Youtube/ver0.4_timer/timer.js
+++ JS_Youtube/ver0.4_timer/timer.js
@@ -3,46 +3,46 @@
 // 終了時刻
 let finish;
 
-// インターバルID
+// 4-1, インターバルID
 let intervalID;
 
-// 開始ボタン
+// 1-1, 開始ボタン
 let startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", startTimer);// タイマー開始
 console.log(startButton);
 
-// 終了ボタン
+// 2-1, 終了ボタン
 let stopButton = document.querySelector("#stop-button");
 stopButton.addEventListener("click", stopTimer);// タイマー終了
 console.log(stopButton);
 
-// タイマーを開始する
+// 1-2, タイマーを開始する
 function startTimer(){
 	console.log("startTimer");
 
-	// フォームから値を取得
+	// 3-1, フォームから値を取得
 	let second = document.querySelector("#time-input").value;
-	// 終了時刻をグローバル変数に保存
+	// 3-2, 終了時刻をグローバル変数に保存
 	finish = Date.now() + second * 1000;
-	// インターバルIDをグローバル変数に保存
+	// 4-2, インターバルIDをグローバル変数に保存
 	intervalID = setInterval(checkRemainingTime, 100);
 
 	// 開始ボタンを無効にする
 	startButton.disabled = true;
 }
 
-// タイマーを終了する
+// 2-2, タイマーを終了する
 function stopTimer(){
 	console.log("stopTimer");
 
-	// インターバルIDを指定してタイマーを停止
+	// 3-3, インターバルIDを指定してタイマーを停止
 	clearInterval(intervalID);
 
 	// 開始ボタンを有効にする
 	startButton.disabled = false;
 }
 
-// 残り時間をチェックする繰り返し
+// 4-3, 残り時間をチェックする繰り返し
 function checkRemainingTime(){
 	// 残り時間を計算する
 	let remain = finish - Date.now();
---
+++ Vue-js/ver0.2_clock/system/js/main.js
@@ -0,0 +1,72 @@
+// Template
+const tmpClock = 
+'<div class="uk-grid-small uk-child-width-auto" uk-grid>' +
+	'<div>' + 
+		'<div class="uk-countdown-number">{{ h }}</div>' +
+		'<div class="uk-countdown-label uk-margin-small uk-text-center">Hours</div>' +
+	'</div>' + 
+	'<div class="uk-countdown-separator">:</div>' +
+	'<div>' + 
+		'<div class="uk-countdown-number">{{ m }}</div>' +
+		'<div class="uk-countdown-label uk-margin-small uk-text-center">Minutes</div>' +
+	'</div>' + 
+	'<div class="uk-countdown-separator">:</div>' +
+	'<div>' + 
+		'<div class="uk-countdown-number">{{ s }}</div>' +
+		'<div class="uk-countdown-label uk-margin-small uk-text-center">Seconds</div>' +
+	'</div>' +
+'</div>';
+
+// MyData
+let myData = {
+	navStr: "Nav",
+	asideStr: "Aside",
+	intervalID: null,
+	timeObj: {
+		h: 0,
+		m: 0,
+		s: 0
+	},
+	msgErrOffline: false
+}
+
+const app = Vue.createApp({
+	data(){
+		return myData;
+	},
+	mounted(){
+		// Online / Offline
+		if(navigator.onLine){
+			console.log("Online");
+			this.msgErrOffline = false;
+		}else{
+			console.log("Offline");
+			this.msgErrOffline = true;
+		}
+		// Start
+		this.start();
+	},
+	methods:{
+		start(){
+			this.intervalID = setInterval(this.update, 200);
+		},
+		stop(){
+			clearInterval(this.intervalID);
+		},
+		update(){
+			const dateObj = new Date();
+			this.timeObj.h = ("0" + dateObj.getHours()).slice(-2);
+			this.timeObj.m = ("0" + dateObj.getMinutes()).slice(-2);
+			this.timeObj.s = ("0" + dateObj.getSeconds()).slice(-2);
+		}
+	}
+});
+
+// Component
+app.component("c_clock", {
+	props: ["h", "m", "s"],
+	template: tmpClock
+});
+
+// Mount
+app.mount("#app");
