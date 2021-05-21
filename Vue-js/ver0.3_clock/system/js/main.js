de9e5760db2033dbf045267dd1a6cd6cc002f429
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-05-21 09:18:51 +0900

    Clock

---
+++ Vue-js/ver0.3_clock/system/js/main.js
@@ -0,0 +1,49 @@
+// MyData
+let myData = {
+	navStr: "Nav",
+	asideStr: "Aside",
+	intervalID: null,
+	timeObj: {h: 0, m: 0, s: 0},
+	msgErrOffline: false
+}
+
+const app = Vue.createApp({
+	data(){
+		return myData;
+	},
+	mounted(){
+		// Online / Offline
+		if(navigator.onLine){
+			console.log("Online");
+			this.msgErrOffline = false;
+		}else{
+			console.log("Offline");
+			this.msgErrOffline = true;
+		}
+		// Start
+		this.start();
+	},
+	methods:{
+		start(){
+			this.intervalID = setInterval(this.update, 200);
+		},
+		stop(){
+			clearInterval(this.intervalID);
+		},
+		update(){
+			const dateObj = new Date();
+			this.timeObj.h = ("0" + dateObj.getHours()).slice(-2);
+			this.timeObj.m = ("0" + dateObj.getMinutes()).slice(-2);
+			this.timeObj.s = ("0" + dateObj.getSeconds()).slice(-2);
+		}
+	}
+});
+
+// Component
+app.component("c_clock", {
+	props: ["h", "m", "s"],
+	template: "#t_clock"
+});
+
+// Mount
+app.mount("#app");
