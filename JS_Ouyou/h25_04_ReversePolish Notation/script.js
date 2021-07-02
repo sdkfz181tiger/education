d346f42de54b2c2cf9e25086c3dd9b463a07de10
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-07-02 18:45:47 +0900

    逆ポーランド記法とフロイドの循環少数

---
+++ JS_Ouyou/h25_04_ReversePolish Notation/script.js
@@ -0,0 +1,63 @@
+console.log("Hello, JavaScript!!");
+
+// H25春_午後問3
+//    逆ポーランド記法
+
+window.onload = (event)=>{
+
+	let str = "1*((2+3)/4)";// 変換前数式文字データ
+	let arr = convertToArr(str);// 変換前配列データ
+	console.log(arr);
+
+	let stack  = [null];// スタック用配列
+	let result = [];// 変換後数式を格納する配列
+
+	for(let i=0; i<arr.length; i++){
+
+		let p = getPriority(arr[i]);// 配列にある値を取り出す
+		if(p < 0) break;// -1の場合はエラーなので停止
+
+		let last = stack.length - 1;// スタックの先頭にアクセス
+		// 変換前配列データの優先度とスタックの先頭の優先度を比較する
+		while(p<=getPriority(stack[last]) && stack[last] != "("){
+			result.push(stack[last]);// スタックの優先度が高い場合はpopする
+			stack = stack.slice(0, -1);// スタックから削除
+			last--;// 優先度が低くなるか、"("でない限り繰り返す
+		}
+
+		if(arr[i] == ")"){// ")"の場合はスタックの先頭"("を削除
+			stack = stack.slice(0, -1);
+		}else{
+			stack.push(arr[i]);// 違う場合はスタックにpushする
+		}
+		// データを確認する
+		console.log(arr[i] + ":" + stack.join(" ") + " -> " + result.join(" "));
+	}
+	// 残りのデータを変換後データに写していく
+	for(let i=stack.length-1; 0<=i; i--) result.push(stack[i]);
+
+	// 結果を確認する
+	console.log(str, " -> ", result.join(" "));
+}
+
+// 文字列を配列に変換する
+function convertToArr(s){
+	let elements = s.split(/(\d{1,}|\+|\-|\*|\-|\(|\))/);
+	let data = [];
+	for(let element of elements){
+		if(element == "") continue;
+		data.push(element);
+	}
+	return data;
+}
+
+// 文字の優先度を取得
+function getPriority(c){
+	if (c === "(") return 5;
+	if (!Number.isNaN(parseInt(c))) return 4;
+	if (c === "*" || c === "/") return 3;
+	if (c === "+" || c === "-") return 2;
+	if (c === ")") return 1;
+	if (c === null) return 0;
+	return -1;// Error
+}
---
+++ JS_Ouyou/h26_04_floid/script.js
@@ -0,0 +1,52 @@
+console.log("Hello, JavaScript!!");
+
+// H26春_午後問3
+//    フロイドの循環検出アルゴリズム
+
+test(56);
+
+function test(n){
+	console.log("Test:"+ n , "計算結果:" + 1 / n);// 0.017857142857142856
+	junkan(n);// 4~9桁目である"857142"が循環小数である
+}
+
+function junkan(n){
+	let m = 1;// カメの位置
+	let p = 1;// ウサギの位置
+	let s = 0;// 循環の先頭
+	let t = 0;// 循環の末尾
+	// 余りが一致するまで
+	while(true){
+		m = amari(m * 10, n);// カメは1歩
+		p = amari(amari(p * 10, n) * 10, n);// ウサギは2歩
+		if(m == p) break;
+	}
+	//console.log("一致:" + m + " <-> " + p);
+	if(p != 0){
+		// 循環の先頭を検出
+		p = 1;// ウサギをリセット
+		s = 1;// 循環の先頭を先頭の位置と一致させる
+		while(m != p){// カメ値とウサギ値が一致するまで
+			s++;
+			m = amari(m * 10, n);// カメは1歩
+			p = amari(p * 10, n);// ウサギは1歩
+		}
+		//console.log("一致:" + m + " <-> " + p);
+		console.log("循環の先頭:" + s);
+		// 循環の末尾を検出
+		p = amari(p * 10, n);// ウサギは1歩
+		t = s;// 循環の末尾を先頭の位置と一致させる
+		while(m != p){// カメ値とウサギ値が一致するまで
+			t++;
+			p = amari(p * 10, n);// ウサギは1歩
+		}
+		//console.log("一致:" + m + " <-> " + p);
+		console.log("循環の末尾:" + t);
+		return;
+	}
+	console.log(n + "は循環小数ではありません");
+}
+
+function amari(a, b){
+	return a % b;
+}
