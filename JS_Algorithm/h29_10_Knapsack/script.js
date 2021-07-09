672a037f213e33558878fbb05af7b1a0974d3411
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-07-09 14:43:51 +0900

    ナップザック問題、ナイト巡回問題

---
+++ JS_Algorithm/h29_10_Knapsack/script.js
@@ -0,0 +1,53 @@
+console.log("Hello, JavaScript!!");
+
+// H29秋 午後問3
+//    ナップザック問題
+
+const N = 4;// 要素の種類
+const W = 10;// 容量制限
+
+const weights = [2, 3, 4, 5];
+const values  = [3, 4, 5, 9];
+
+// NxW+1の配列
+let table = Array.from(new Array(N), ()=>new Array(W+1).fill(-1));
+
+window.onload = (event)=>{
+
+	// ナップザック問題
+	for(let n=0; n<N; n++){
+		for(let w=0; w<W+1; w++){
+			calc(n, w);
+		}
+	}
+
+	console.table(table);// 結果を確認
+}
+
+function calc(n, w){
+	if(table[n][w] != -1) return table[n][w];// 確定済み
+
+	if(w < getWeight(n)){
+		table[n][w] = getDp(n-1, w);// 容量をオーバーしていた場合
+	}else{
+		let i = getWeight(n);
+		let aV = getDp(n-1, w);// 追加しなかった場合
+		let bV = getDp(n, w-i) + values[n];// 追加した場合
+		table[n][w] = Math.max(aV, bV);// 比較して価値がより高い方を採用
+	}
+}
+
+function getWeight(w){
+	if(w < 0) return 0;
+	return weights[w];
+}
+
+function getValue(v){
+	if(v < 0) return 0;
+	return values[v];
+}
+
+function getDp(n, w){
+	if(n < 0 || w < 0) return 0;
+	return table[n][w];
+}
---
+++ JS_Algorithm/h30_04_Knight/script.js
@@ -0,0 +1,42 @@
+console.log("Hello, JavaScript!!");
+
+// H30春 午後問3
+//    ナイト巡回問題
+
+const V = 4;// 盤面の行数
+const H = 3;// 盤面の列数
+
+// VxHの配列
+let board = Array.from(new Array(V), ()=>new Array(H).fill(0));
+
+// ナイトが移動する方向(8方向)
+const dV = [-2, -1,  1, 2,  2,  1, -1, -2];
+const dH = [1,   2,  2, 1, -1, -2, -2, -1];
+
+window.onload = (event)=>{
+
+	// ナイト巡回問題
+	search(1, 0, 0);
+}
+
+function search(i, v, h){
+	if(v < 0 || V-1 < v) return;// 盤面の外であったら中止
+	if(h < 0 || H-1 < h) return;
+	if(board[v][h] != 0) return;// 巡回済みであったら中止
+	board[v][h] = (i<10) ? "0"+i:i;// 巡回の番号
+
+	if(V*H <= i){
+		show();// 最後に到達していた場合は書き出す
+		return;
+	}else{
+		// ナイトの8方向それぞれのマスを調べる
+		for(let j=0; j<dV.length; j++){
+			search(i+1, v+dV[j], h+dH[j]);
+		}
+	}
+	board[v][h] = 0;// マスをリセットする
+}
+
+function show(){
+	for(let line of board) console.log(line.join(","));
+}
