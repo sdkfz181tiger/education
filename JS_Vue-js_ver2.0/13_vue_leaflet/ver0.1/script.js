967537eb93fc4887812e1995d06d667f43e9ba0d
ShimejiOzaki <sdkfz181tiger@gmail.com>
2021-07-15 12:33:43 +0900

    Vue.jsでLeaflet

--- JS_Vue-js_ver2.0/13_vue_leaflet/ver0.1/script.js
+++ JS_Vue-js_ver2.0/13_vue_leaflet/ver0.1/script.js
@@ -1,5 +1,11 @@
 // Vue.js
 
+// Leaflet
+const ATTRIBUTION = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const ACCESS_TOKEN = "pk.eyJ1Ijoic2RrZnoxODF0aWdlciIsImEiOiJja3FxNGU0cmcwdWFoMnhxaG5mcDYyaWwzIn0.acxWamqlCUkmAATOIUTlAQ";
+const POSITION = [35.3839023, 136.6083425];
+
+// SpreadSheet
 const URL_GOOGLE = "https://docs.google.com/spreadsheets/d/e/";
 const URL_HASH = "2PACX-1vQTNtYa2s1mYs9S2pnrm9OzE0iUrzorKNp1hy_M35hYiyQaIL-XtOSqbkd2ccx_EpXoDOa21ckCE_nK/";
 const URL_CSV = "pub?gid=0&single=true&output=csv";
@@ -16,13 +22,19 @@ function createApp(){
 			items: [],
 		},
 		mounted:function(){
-			// CSV
-			axios.get(URL, OPTION).then(res=>{
-				res.data.text().then(str=>{
-					this.items = this.convertToArr(str);
-					console.table(this.items);
-				});
-			});
+			// Leaflet
+			let map = L.map("mapid").setView(POSITION, 16);
+			L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+				attribution: ATTRIBUTION,
+				accessToken: ACCESS_TOKEN,
+				id: "mapbox/streets-v11",
+				tileSize: 512,
+				maxZoom: 18,
+				zoomOffset: -1
+			}).addTo(map);
+			// Marker
+			L.marker(POSITION).addTo(map)
+			.bindPopup("Hello OpenStreetMap!!").openPopup();
 		},
 		methods:{
 			connect:function(){
